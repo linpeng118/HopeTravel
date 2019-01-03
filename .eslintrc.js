f90bcbb8264d548e98fabd85119b99e38711f080
c_illusion <670199384@qq.com>
2019-01-03 13:53:14 +0800

    修改eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/html-indent': 0,
     'vue/max-attributes-per-line': 0,
+    'vue/attributes-order': 0,
+    'vue/name-property-casing': 0
   }
 }
