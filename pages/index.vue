06da29f52eb0eccc3ad22e681e38a0d03885d661
c_illusion <670199384@qq.com>
2019-01-10 13:27:56 +0800

    格式化

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
+      <logo />
       <h1 class="title">
         nuxt
       </h1>
@@ -9,12 +9,10 @@
         My best Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
+        <a href="https://nuxtjs.org/"
           target="_blank"
           class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
+        <a href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey">GitHub</a>
       </div>
@@ -23,63 +21,61 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import {getData} from './m2/api/test'
-import {mapMutations} from 'vuex'
-export default {
-  components: {
-    Logo
-  },
-  data () {
-    return {
-      ss: 'sssss'
+  import Logo from '~/components/Logo.vue'
+  import {getData} from './m2/api/test'
+  import {mapMutations} from 'vuex'
+  export default {
+    components: {
+      Logo
+    },
+    data() {
+      return {
+        ss: 'sssss'
+      }
+    },
+    mounted() {
+      getData().then(res => {
+        console.log(res)
+        window.localStorage.setItem('token', 'token111111111')
+        this.updateToken('token111111111')
+      })
+    },
+    methods: {
+      ...mapMutations([
+        'updateToken'
+      ])
     }
-  },
-  mounted () {
-    getData().then(res => {
-      console.log(res)
-      window.localStorage.setItem('token', 'token111111111')
-      this.updateToken('token111111111')
-    })
-  },
-  methods: {
-    ...mapMutations([
-      'updateToken'
-    ])
   }
-}
 </script>
 
 <style>
+  .container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .title {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+      "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-
+  .links {
+    padding-top: 15px;
+  }
 </style>
