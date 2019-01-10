93d38ab77923b7dd08d8f9bb33ef6d1cc4a6e2b1
c_illusion <670199384@qq.com>
2019-01-10 10:41:34 +0800

    修改配置文件

--- pages/m2/assets/js/common/config.js
+++ pages/m2/assets/js/common/config.js
@@ -1,2 +1,1 @@
 export const TOKEN_KEY = 'token'
-export const CODE_OK = 0
--- plugins/axios/index.js
+++ plugins/axios/index.js
@@ -23,7 +23,6 @@ axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded
 axios.interceptors.request.use(
   config => {
     // 可以设置请求头参数
-    console.log(1, store)
     const token = store().getters.token;
     if (token) {
       config.headers[TOKEN_KEY] = token // 请求头带上token
@@ -36,21 +35,29 @@ axios.interceptors.request.use(
 )
 
 // 相应拦截器
-axios.interceptors.response.use(
-  response => {
-    // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据     
-    // 否则的话抛出错误
-    if (response.status === 200) {
-      return Promise.resolve(response);
-    } else {
-      return Promise.reject(response);
-    }
-  },
+instance.interceptors.response.use(
+  // 请求成功
+  res => res.status === 200 ? Promise.resolve(res) : Promise.reject(res),
+  // 请求失败
   error => {
-    return Promise.resolve(error.response)
+    const {
+      response
+    } = error;
+    if (response) {
+      // 请求已发出，但是不在2xx的范围 
+      errorHandle(response.status, response.data.msg);
+      return Promise.reject(response);
+    } else {
+      // 处理断网的情况
+      // eg:请求超时或断网时，更新state的network状态
+      // network状态在app.vue中控制着一个全局的断网提示组件的显示隐藏
+      // 关于断网组件中的刷新重新获取数据，会在断网组件中说明
+      // store.commit('changeNetwork', false);
+    }
   }
 )
 
+// 创建一个请求
 const httpServer = (param) => {
   console.log(11, param)
   var defaults = {
@@ -81,7 +88,38 @@ const httpServer = (param) => {
     })
   })
 }
+
 export default httpServer
+
+/** 
+ * 请求失败后的错误统一处理 
+ * @param {Number} status 请求失败的状态码
+ */
+const errorHandle = (status, other) => {
+  // 状态码判断
+  switch (status) {
+    // 401: 未登录状态，跳转登录页
+    case 401:
+      // toLogin();
+      break;
+      // 403 token过期
+      // 清除token并跳转登录页
+    case 403:
+      tip('登录过期，请重新登录');
+      // localStorage.removeItem('token');
+      // store.commit('loginSuccess', null);
+      // setTimeout(() => {
+      //   toLogin();
+      // }, 1000);
+      break;
+      // 404请求不存在
+    case 404:
+      tip('请求的资源不存在');
+      break;
+    default:
+      console.log(other);
+  }
+}
 
 /** 
  * 提示函数 
@@ -100,7 +138,7 @@ const tip = msg => {
  * 携带当前页面路由，以期在登录页面完成登录后返回当前页面
  */
 const toLogin = () => {
-  router.replace({
+  this.$router.replace({
     path: '/login',
     query: {
       redirect: router.currentRoute.fullPath
