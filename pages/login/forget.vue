9d283adb7781dc9d4b459174774a2f4e48dcf787
c_illusion <670199384@qq.com>
2019-01-12 15:03:18 +0800

    登陆页面样式ok

---
+++ pages/login/forget.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>忘记密码</div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data() {
+      return {
+      }
+    },
+    computed: {},
+    mounted() {},
+    methods: {},
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -2,43 +2,96 @@
   <div class="login-page">
     <h1 class="title">稀饭旅行账号登陆</h1>
     <van-tabs class="login-wrap tours-tabs-nowrap">
-      <van-tab title="普通登陆">
-        <van-field v-model="username"
-          placeholder="请输入用户名" />
-        <van-field v-model="password"
-          center
-          clearable
-          placeholder="请输入密码">
-          <van-button slot="icon"
-            size="small"></van-button>
-          <van-button slot="button"
-            size="small"
-            type="primary">忘记密码</van-button>
-        </van-field>
+      <!-- 普通登陆 -->
+      <van-tab class="login"
+        title="普通登陆">
+        <van-cell-group>
+          <van-field class="username tours-input"
+            v-model="username"
+            placeholder="请输入用户名" />
+          <van-field class="password tours-input"
+            v-model="password"
+            center
+            clearable
+            icon="eye-o"
+            placeholder="请输入密码"
+            :type="pswInputType"
+            @click-icon="toggleInputType">
+            <van-button class="tours-button-noborder"
+              slot="button"
+              size="small"
+              @click="forgetPsw">忘记密码</van-button>
+          </van-field>
+        </van-cell-group>
+        <van-button class="btn-login tours-button"
+          size="large">登陆</van-button>
       </van-tab>
-      <van-button size="large">登陆</van-button>
-      <p>登陆即代表您已同意我们的<span>服务协议</span></p>
-      <van-tab title="手机验证码登陆">
-        手机验证码登陆
+      <!-- 手机验证码登陆 -->
+      <van-tab class="mobile-login"
+        title="手机验证码登陆">
+        <van-cell-group>
+          <area-code-input class="mobile" />
+          <van-field class="auth-code tours-input"
+            v-model="authCode"
+            center
+            clearable
+            placeholder="请输入验证码">
+            <van-button class="tours-button-noborder"
+              slot="button"
+              size="small"
+              @click="getCode">获取验证码</van-button>
+          </van-field>
+        </van-cell-group>
+        <van-button class="btn-login tours-button"
+          size="large">登陆</van-button>
       </van-tab>
+      <p class="text">登陆即代表您已同意我们的<span>&nbsp;服务协议</span></p>
     </van-tabs>
   </div>
 </template>
 
 <script>
+  import AreaCodeInput from '@/components/input/areaCode'
+
   export default {
-    components: {},
+    components: {
+      AreaCodeInput
+    },
     data() {
       return {
+        // 用户登录
         username: '',
         password: '',
+        pswInputType: 'password', // 密码输入框类型
+        // 手机登录
+        mobile: '',
+        authCode: '', // 验证码
+        areaCode: '86', // 区号
+        show: false,
       }
     },
     computed: {},
     mounted() {},
-    methods: {},
+    methods: {
+      toggleInputType(val) {
+        this.pswInputType = this.pswInputType === 'password' ? 'text' : 'password'
+      },
+      forgetPsw() {
+        this.$router.push({
+          path: '/login/forget'
+        })
+      },
+      // 获取验证码
+      getCode() {
+        // TODO:
+      },
+      onAreaCode() {
+        console.log(123)
+      }
+    },
   }
 </script>
+
 
 <style lang="scss" scoped>
   .login-page {
@@ -54,6 +107,43 @@
     }
     .login-wrap {
       margin-top: 100px;
+      .login,
+      .mobile-login {
+        padding: 0 76px;
+        .icon-arrow {
+          position: absolute;
+          top: 0;
+          left: 50px;
+          z-index: 999;
+        }
+        .btn-login {
+          margin-top: 20px;
+        }
+      }
+      .login {
+        .username {
+          margin-top: 54px;
+        }
+        .password {
+          margin-top: 16px;
+        }
+      }
+      .mobile-login {
+        .mobile {
+          margin-top: 54px;
+        }
+        .auth-code {
+          margin-top: 16px;
+        }
+      }
+      .text {
+        margin-top: 30px;
+        font-size: 22px;
+        color: #c4c4c4;
+        span {
+          color: rgba(57, 158, 246, 1);
+        }
+      }
     }
   }
 </style>
