88e22a412df92c5e4b913fd4c1d906b3fd56b739
litu <tutu_8936064@qq.com>
2019-01-15 18:14:43 +0800

    custom

---
+++ pages/custom/gushi3.vue
@@ -0,0 +1,293 @@
+<template>
+  <div>
+    <div class="banner">
+      <a class="return" @click="goBack" href="javascript:;"></a>
+      <span class="title">旅行故事</span>
+      <span class="site">目的地：日本</span>
+    </div>
+    <div class="ct">
+      <h5 class="ct_h5">青春时期做过的梦，
+        <br>都在镰仓的海风里实现
+      </h5>
+      <p class="paragraph">
+        一直就非常想去日本，比起东南亚一些国家，日本签证会稍微麻烦一点。不想跟着旅行大团匆匆忙忙的景点拍照，想要真正的踏实的去追寻属于自己的青春记忆。
+        <br>从成都出发，性价比超高的川航成为首选。计划是从双流机场直飞东京的成田，然后在日本境内做新干线到大阪，最后从大阪返回国内。不得不说，去的时候川航飞机上的小吃零食简直太窝心了，好吃到我差点不想下飞机。
+        <strong>【东京篇】</strong>
+      </p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p1@2x.png"
+          alt
+        >
+      </div>
+      <p
+        class="paragraph"
+      >日本的感觉和国内真的对比特别明显，感觉所有的人都在尽力保证不去做一些打扰到别人的举动，尤其是搭乘公共交通的时候，打电话是极其不礼貌的举动，所以我都有默默把手机调整成静音。
+        <br>路上熙熙攘攘的上班族，超级干净的马路，配上现代化的建筑，彬彬有礼的服务体验，让你瞬间融入这种环境。
+      </p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p2@2x.png"
+          alt
+        >
+      </div>
+      <p class="paragraph">
+        <strong>作为漫迷，来东京最不能错过的一个地方就是——秋叶原。</strong>
+        刚到东京的时候，总觉得看着清清冷冷的街道，会让人有点冷淡的感觉，可以进入秋叶原之后，全部都是迎面而来的卡哇伊！浓浓的二次元风在这里炸裂，满大街都是周边、手办，我要羡慕疯了！在这里买的手办，其实也不一定会比线上平台买的便宜，但是重点是这样琳琅满目都是手办的购物感觉，那种一个个鲜活的的手办就在眼前，比网络上冷冰冰的图片来的更让我这个漫迷激动了！
+      </p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p3@2x.png"
+          alt
+        >
+      </div>
+      <p class="paragraph">
+        <strong>富士山可以说是日本必去的景点。</strong>
+        富士山，东京附近很多地方都能观赏。作为日本国内的最高峰，被日本誉为“圣岳”，所以选择特别接近富士山的河口湖。尤其是清晨的时候，我看着光亮一点点的漫过山顶照亮了山腰，接着全世界都开始苏醒过来，那一刻内心好平静。
+        <br>围绕着河口湖，稀饭旅行的SKY特意推荐我们租了一个自行车骑行河口湖，在湖边骑行，微风飘过，看见美景可以随时停下来拍照，特别有一种小清新的感觉，可惜当时樱花未开，不然整个湖边遍地樱花，更是别有一番风味。
+      </p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p4@2x.png"
+          alt
+        >
+      </div>
+      <p class="paragraph">
+        <strong>富士山可以说是日本必去的景点。</strong>
+        富士山，东京附近很多地方都能观赏。作为日本国内的最高峰，被日本誉为“圣岳”，所以选择特别接近富士山的河口湖。尤其是清晨的时候，我看着光亮一点点的漫过山顶照亮了山腰，接着全世界都开始苏醒过来，那一刻内心好平静。
+        <br>围绕着河口湖，稀饭旅行的SKY特意推荐我们租了一个自行车骑行河口湖，在湖边骑行，微风飘过，看见美景可以随时停下来拍照，特别有一种小清新的感觉，可惜当时樱花未开，不然整个湖边遍地樱花，更是别有一番风味。
+        <strong>【镰仓篇】</strong>
+      </p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p5@2x.png"
+          alt
+        >
+      </div>
+      <p
+        class="paragraph"
+      >此次最期待的一站——镰仓。镰仓是神奈川的一个普通小城，但对很多人来说这里就是圣地，于我也是意义非凡。前段时间看了电影《镰仓物语》，一下子就被那熟悉的街景戳中，成为了我这次日本之行的动力之一。
+        <br>来到镰仓，最标志性的建筑估计高德院可以排到榜首，无论你是通过怎么样的交通方式抵达镰仓，都会被一尊大佛吸引眼球，这座镰仓大佛是必去的打卡地之一！其实大佛的内部是空心结构呢，所以游客是可以从佛像的背后的底部进入参观的。
+        <br>没有人会拒绝镰仓高校前站这个网红打卡地，就像没有人会拒绝《灌篮高手》一样，虽然我们都知道湘北最后并没有获得全国冠军，可是丝毫不能阻止它在我们青春里的位置。
+      </p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p6@2x.png"
+          alt
+        >
+      </div>
+      <p class="paragraph">
+        不得不说因为《灌篮高手》，这个平凡的小车站变成了我们这有的漫迷的朝圣地，坐着海边行驶的江之电，我耳机里放着《直到世界尽头》，樱木与晴子打招呼的车站，流川枫骑车路过的海岸，还有镰仓高校(湘北高校原型).....远远眺望着海边的风光，总觉得打破了次元壁，被这个恬静的地方治愈了。
+        <strong>【奈良篇】</strong>
+      </p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p7@2x.png"
+          alt
+        >
+      </div>
+      <p
+        class="paragraph"
+      >大部分去日本玩的朋友都会纠结去关东还是关西，幸运的是我的时间允许，小开心一下。远离东京的繁华，进入关西区域就古都韵味厚重很多，我感觉关西才是日本特色最浓郁的地方。
+        <br>一进入奈良公园，就看见了传说中的神鹿。完全不害怕人们的亲近，很多游客和小鹿拍照，公园大马路上都有梅花鹿的影子，车子一般给梅花鹿让行。游客可以购买鹿饼来喂鹿，一捆鹿饼150日元。
+      </p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p8@2x.png"
+          alt
+        >
+      </div>
+      <p
+        class="paragraph"
+      >需要注意的是，小鹿的性格有可能是非常迥异的，千万不要主观意愿的觉得所有的小鹿都是温柔可爱的，有的还会去抢鹿饼吃。如果你手上有鹿饼，一群鹿就会蜂拥而来，围着你要吃的，你要是不给，它们会咬你的衣服，在你衣服上留下黄褐色的口水。当然这里也有很多可爱的小鹿，拍出来的照片也很有灵气，听说整个奈良，大概有1500多头梅花鹿。</p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p9@2x.png"
+          alt
+        >
+      </div>
+      <p class="paragraph">
+        <strong>【京都篇】</strong>
+      </p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p10@2x.png"
+          alt
+        >
+      </div>
+      <p
+        class="paragraph"
+      >京都，是我这次去日本感触最深的城市，虽然是奔着镰仓来的，但是没想到京都的美丽与典雅让我为之着迷。在京都，一套和服是很有必要的。女生穿戴好合服后，一定要在做一个盘发的造型，配一点精美的配饰，这样一装扮就可以在这座很有魅力的城市留下你最美的旅行照片了。</p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p11@2x.png"
+          alt
+        >
+      </div>
+      <p class="paragraph">
+        走在京都的小巷种，会让你感觉，恐怕再也没有哪座城市像京都一样拥有如此众多幽静的古刹与神社，惊人的古建密度。但京都又有入世的一面，无论在花见小路与盛装的艺伎擦肩而过，或是观看一场歌舞伎表演，都能让你体会京都的世俗风情。
+        <strong>【大阪篇】</strong>
+      </p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p12@2x.png"
+          alt
+        >
+      </div>
+      <p
+        class="paragraph"
+      >大阪城公园真的是一个超级大的公园，不过来这个公园主要还是想想看天守阁，位于公园的中间了，天公作美阳光明媚，拍出来的照片都非常美，衬托的建筑格外宏伟！</p>
+      <div>
+        <img
+          style="width:6.68rem;height:3.64rem;margin-top:.2rem;"
+          src="../../assets/imgs/custom/story3/p13@2x.png"
+          alt
+        >
+      </div>
+      <p class="paragraph">这一次的日本之旅在大阪画上了句号，原本我是奔着镰仓去圆梦，没有想到被一路日本文化触动，从关东到关西这样的打跨度的旅行行程基本上是随了我自己的心意。
+        <br>我想，旅行的意义不是逃避，而是去追逐更多想象意外的世界。
+        <br>你永远都不知道，你的自以为是多么幼稚。
+        <br>所以，不困于眼前去看看世界多么重要，愿以后还能去更多的地方，看更多的风景！
+      </p>
+    </div>
+
+    <div class="btn_container clearfix">
+      <a
+        target="_blank"
+        href="http://p.qiao.baidu.com/cps/chat?siteId=12918104&userId=26301226"
+        class="btn_online fl"
+        data-ajax="false"
+        id="qa-wangqiao-btn"
+      >
+        <img style="widht:.52rem;height:.52rem;" src="../../assets/imgs/custom/btn1.png" alt>
+        在线咨询
+      </a>
+      <a href="/custom/index" class="btn_custom fl">
+        <img style="widht:.48rem;height:.48rem;" src="../../assets/imgs/custom/btn2.png" alt>
+        立即定制
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "component_name",
+  data() {
+    return {};
+  },
+  methods: {
+    goBack() {
+      window.history.go(-1);
+    }
+  }
+};
+</script>
+<style>
+body {
+  margin: 0;
+  color: #404040;
+}
+.ct img {
+  width: 100% !important;
+  height: auto !important;
+}
+.banner {
+  background: url("../../assets/imgs/custom/story3/banner.png") no-repeat center
+    center/750px 708px;
+  width: 750px;
+  height: 708px;
+  position: relative;
+}
+.return {
+  position: absolute;
+  top: 20px;
+  left: 0px;
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  background: url("../../assets/imgs/custom/back.png") no-repeat center
+    center/24px 41px;
+}
+.title {
+  position: absolute;
+  top: 85px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: inline-block;
+  font: 34px/36px "";
+  color: #fff;
+}
+.site {
+  position: absolute;
+  bottom: 23px;
+  left: 30px;
+  display: inline-block;
+  padding: 0 20px;
+  height: 50px;
+  background: #fff;
+  opacity: 0.8;
+  border-radius: 5px;
+  text-align: center;
+  font: 30px/50px "";
+  color: #000;
+}
+.ct {
+  padding: 14px 32px;
+  position: relative;
+  padding-bottom: 120px;
+  background: #fff;
+}
+.ct_h5 {
+  font: 36px/46px "";
+  color: #356371;
+}
+.paragraph {
+  font: 26px/40px "";
+  color: #484848;
+  margin: 10px 0;
+  text-align: justify;
+  margin: 20px 0;
+}
+.btn_container {
+  width: 750px;
+  height: 100px;
+  position: fixed;
+  bottom: 0;
+  background: #fff;
+}
+.btn_container a {
+  width:375px;
+  display: inline-block;
+  height: 100px;
+  box-sizing: border-box;
+  text-align: center;
+}
+.btn_online {
+  color: #5e5e5e !important;
+  background: rgb(243, 243, 243);
+  font: 32px/100px "";
+}
+.btn_custom {
+  background: #ef9a1a;
+  color: #fff !important;
+  font: 32px/100px "";
+}
+strong {
+  color: #000;
+  font-size: 28px;
+  margin: 20px 0 10px;
+  display: block;
+}
+</style>
---
+++ pages/custom/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      hahaha
+  </div>
+</template>
+<script>
+export default {
+  name: "component_name",
+  data() {
+    return {};
+  }
+};
+</script>
+
+
+
+
+
+<style lang="scss" scoped>
+
+
+
+</style>
