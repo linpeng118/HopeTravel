53636132261dedba96befe4e3d885416fdcd8b3d
c_illusion <670199384@qq.com>
2019-01-29 15:54:51 +0800

    修复setting页面

--- pages/personal/setting.vue
+++ pages/personal/setting.vue
@@ -35,10 +35,10 @@
       return {
         settingList: [
           {type: SETTING.CHANGE_PSW, icon: require('~/assets/imgs/personal/setting_1@2x.png'), title: '修改密码', isRightIcon: true, value: '', path: '/personal/changePsw'},
-          {type: SETTING.ABOUT, icon: require('~/assets/imgs/personal/setting_2@2x.png'), title: '关于稀饭', isRightIcon: true, value: '', path: '/protocol/about'},
-          {type: SETTING.VERSION, icon: require('~/assets/imgs/personal/setting_3@2x.png'), title: '当前系统版本', isRightIcon: false, value: 'V1.0', path: '/personal/version'},
-          {type: SETTING.GRADE, icon: require('~/assets/imgs/personal/setting_4@2x.png'), title: '给我们评分', isRightIcon: true, value: '', path: '/personal/grade'},
-          {type: SETTING.CLEAR_CACHE, icon: require('~/assets/imgs/personal/setting_5@2x.png'), title: '清除缓存', isRightIcon: true, value: '', path: '/personal/clearCache'},
+          {type: SETTING.ABOUT, icon: require('~/assets/imgs/personal/setting_2@2x.png'), title: '关于稀饭', isRightIcon: true, value: '', path: '/personal/about'},
+          // {type: SETTING.VERSION, icon: require('~/assets/imgs/personal/setting_3@2x.png'), title: '当前系统版本', isRightIcon: false, value: 'V1.0', path: '/personal/version'},
+          // {type: SETTING.GRADE, icon: require('~/assets/imgs/personal/setting_4@2x.png'), title: '给我们评分', isRightIcon: true, value: '', path: '/personal/grade'},
+          // {type: SETTING.CLEAR_CACHE, icon: require('~/assets/imgs/personal/setting_5@2x.png'), title: '清除缓存', isRightIcon: true, value: '', path: '/personal/clearCache'},
         ]
       }
     },
