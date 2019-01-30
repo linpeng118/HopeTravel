a3fcb408231bee81cc3a1e824a1b27ae345e7419
c_illusion <670199384@qq.com>
2019-01-30 18:11:42 +0800

    修改index

--- plugins/axios/index.js
+++ plugins/axios/index.js
@@ -42,7 +42,7 @@ export default function ({
   $axios.onError(error => {
     const code = parseInt(error.response && error.response.status)
     if (code === 400) {
-      redirect('/400')
+      console.log('400')
     }
   })
 }
