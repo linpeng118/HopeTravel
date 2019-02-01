ec4e0e10d381897394fe2d5e40016d3682d655cb
c_illusion <670199384@qq.com>
2019-02-01 16:18:13 +0800

    还原地址

--- nuxt.config.js
+++ nuxt.config.js
@@ -104,7 +104,7 @@ module.exports = {
   proxy: {
     // 配置代理
     '/api': {
-      target: 'http://api.tourscool.com/api/tour/v1', // api
+      target: 'http://api.qa.tourscool.net/api/tour/v1', // api
       pathRewrite: {
         '^/api': '/',
       },
