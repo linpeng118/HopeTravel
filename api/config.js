746b4fdcbe857436ad0ca708dab8a7cf4ab7841e
c_illusion <670199384@qq.com>
2019-02-12 11:27:17 +0800

    添加预发布环境

--- api/config.js
+++ api/config.js
@@ -9,6 +9,12 @@ const qa = {
   payment: 'http://www.htw.tourscool.net',
 }
 
+// 预发布
+const pre = {
+  base: 'http://api.tourscool.net',
+  payment: 'http://www.htw.tourscool.net',
+}
+
 const master = {
   base: 'http://api.tourscool.com',
   payment: 'http://www.htw.tourscool.com',
@@ -16,6 +22,8 @@ const master = {
 
 if (process.env.ENV_TYPE === 'prodMaster') {
   apiPath = master
+} else if (process.env.ENV_TYPE === 'prodPre') {
+  apiPath = pre
 } else if (process.env.ENV_TYPE === 'prodQa') {
   apiPath = qa
 } else {
