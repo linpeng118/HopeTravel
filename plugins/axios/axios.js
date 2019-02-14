0fca57f3f3a88850dc0c8946f89ed16e63cd22b6
c_illusion <670199384@qq.com>
2019-02-14 14:19:53 +0800

    修复登录跳转问题

--- plugins/axios/axios.js
+++ plugins/axios/axios.js
@@ -53,7 +53,7 @@ httprequest.interceptors.response.use(
   res => {
     if (res.status === 200) {
       // 700：必须重新登录；401：返回了新的token
-      if(res.data.code === 700 && res.config.url.indexOf('profile') < 0) {
+      if (res.data.code === 700 && res.config.url.indexOf('profile') < 0) {
         window.location.href = '/login?redirect=' + encodeURIComponent(window.location.pathname + window.location.search)
       } else if (res.data.code === 401) {
         const token = res.data.data.token
