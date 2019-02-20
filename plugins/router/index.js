0696829a5d266acfd652f1878dcf630013772ea4
于雪花 <yuxuehua@tourscool.com>
2019-02-20 10:58:21 +0800

    router 更改

--- plugins/router/index.js
+++ plugins/router/index.js
@@ -4,13 +4,6 @@ export default ({ app }) => {
   app.router.beforeEach((to,from, next) => {
     NProgress.start()
     if(to.path.indexOf('.html') >= 0) {
-      console.log(to)
-      // product/detail?productId=1445
-      to.fullPath = 'product/detail?productId=1445'
-      to.path = 'product/detail'
-      to.query = {
-        productId: '1445'
-      }
     }
     next()
   })
