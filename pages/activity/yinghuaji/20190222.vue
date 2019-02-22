72df1b0e7b93a89b7cb6191ddd80fa5005dee859
litu <tutu_8936064@qq.com>
2019-02-22 20:32:58 +0800

    yinghuaji

---
+++ pages/activity/yinghuaji/20190222.vue
@@ -0,0 +1,265 @@
+<template>
+    <div>
+        <div class="section1">   
+        </div>
+        <div class="section2"> 
+            <div class="s-title">
+                <img class="coupon-size" src="../../../assets/imgs/activity/yinghuaji/title/13.png" alt="">
+            </div> 
+            <div class="s-pic">
+                <img class="img1" src="../../../assets/imgs/activity/yinghuaji/title/1.png" alt="">
+            </div>
+            <div class="s-pic">
+                <img class="img2" src="../../../assets/imgs/activity/yinghuaji/title/2.png" alt="">
+            </div>
+            <div class="s-pic">
+                <img class="img3" src="../../../assets/imgs/activity/yinghuaji/protect/1.png" alt="">
+            </div>
+            <div class="pro">
+                <a target="_blank" @click="jump('https://www.tourscool.com/tour2000.html','tourscool://open/LineDetailsVC?productID=2000')">
+                    <img class="img-large" src="../../../assets/imgs/activity/yinghuaji/protect/2.png" alt="">
+                </a>
+            </div>
+            <div class="pro">
+                <a @click="jump('https://www.tourscool.com/tour2407.html','tourscool://open/LineDetailsVC?productID=2407')">
+                    <img class="img-large" src="../../../assets/imgs/activity/yinghuaji/protect/3.png" alt="">
+                </a>
+            </div>
+            <div class="pro">
+                <a @click="jump('https://www.tourscool.com/tour2412.html','tourscool://open/LineDetailsVC?productID=2412')">
+                    <img class="img-large" src="../../../assets/imgs/activity/yinghuaji/protect/4.png" alt="">
+                </a>
+            </div>
+            <div class="pro">
+                <a @click="jump('https://www.tourscool.com/tour2406.html','tourscool://open/LineDetailsVC?productID=2406')">
+                    <img class="img-large" src="../../../assets/imgs/activity/yinghuaji/protect/5.png" alt="">
+                </a>
+            </div>
+            <div class="pro-flex">
+                <a @click="jump('https://www.tourscool.com/tour2409.html','tourscool://open/LineDetailsVC?productID=2409')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/6.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour2410.html','tourscool://open/LineDetailsVC?productID=2410')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/7.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour2002.html','tourscool://open/LineDetailsVC?productID=2002')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/8.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour1973.html','tourscool://open/LineDetailsVC?productID=1973')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/9.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour1400.html','tourscool://open/LineDetailsVC?productID=1400')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/10.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour1896.html','tourscool://open/LineDetailsVC?productID=1896')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/11.png" alt="">
+                </a>
+            </div>
+            <div class="s-pic">
+                <img class="img4" src="../../../assets/imgs/activity/yinghuaji/title/3.png" alt="">
+            </div>
+            <div class="s-pic">
+                <img class="img5" src="../../../assets/imgs/activity/yinghuaji/title/5.png" alt="">
+            </div>
+            <div class="pro-flex">
+                <a @click="jump('https://www.tourscool.com/tour2368.html','tourscool://open/LineDetailsVC?productID=2368')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/12.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour2365.html','tourscool://open/LineDetailsVC?productID=2365')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/13.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour2363.html','tourscool://open/LineDetailsVC?productID=2363')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/14.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour2355.html','tourscool://open/LineDetailsVC?productID=2355')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/15.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour2349.html','tourscool://open/LineDetailsVC?productID=2349')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/16.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour2358.html','tourscool://open/LineDetailsVC?productID=2358')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/17.png" alt="">
+                </a>
+            </div>
+            <div class="s-pic">
+                <img class="img4" src="../../../assets/imgs/activity/yinghuaji/title/3.png" alt="">
+            </div>
+            <div class="s-pic">
+                <img class="img6" src="../../../assets/imgs/activity/yinghuaji/title/4.png" alt="">
+            </div>
+            <div class="pro-flex">
+                <a @click="jump('https://www.tourscool.com/tour48.html','tourscool://open/LineDetailsVC?productID=48')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/18.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour1941.html','tourscool://open/LineDetailsVC?productID=1941')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/19.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour1944.html','tourscool://open/LineDetailsVC?productID=1944')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/20.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour1981.html','tourscool://open/LineDetailsVC?productID=1981')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/21.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour2.html','tourscool://open/LineDetailsVC?productID=2')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/22.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour900.html','tourscool://open/LineDetailsVC?productID=900')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/23.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour666.html','tourscool://open/LineDetailsVC?productID=666')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/24.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/tour667.html','tourscool://open/LineDetailsVC?productID=667')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/25.png" alt="">
+                </a>
+            </div>
+            <div class="s-pic">
+                <img class="img4" src="../../../assets/imgs/activity/yinghuaji/title/3.png" alt="">
+            </div>
+            <div class="s-pic">
+                <img class="img7" src="../../../assets/imgs/activity/yinghuaji/title/6.png" alt="">
+            </div>
+            <div class="pro-flex2">
+                <a @click="jump('https://www.tourscool.com/search?w=%E9%BB%84%E7%9F%B3%E5%B0%8F%E6%9C%A8%E5%B1%8B','tourscool://open/ProductListVC?itemType=1&keyword=%E9%BB%84%E7%9F%B3%E5%B0%8F%E6%9C%A8%E5%B1%8B')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/26.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/search?w=%E5%A4%8F%E5%A8%81%E5%A4%B7','tourscool://open/ProductListVC?itemType=1&keyword=%E5%A4%8F%E5%A8%81%E5%A4%B7')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/27.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/search?w=%E6%B3%B0%E5%9B%BD','tourscool://open/ProductListVC?itemType=1&keyword=%E6%B3%B0%E5%9B%BD')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/28.png" alt="">
+                </a>
+                <a @click="jump('https://www.tourscool.com/search?w=%E6%96%B0%E8%A5%BF%E5%85%B0','tourscool://open/ProductListVC?itemType=1&keyword=%E6%96%B0%E8%A5%BF%E5%85%B0')">
+                    <img class="" src="../../../assets/imgs/activity/yinghuaji/protect/29.png" alt="">
+                </a>
+            </div>
+             <div class="s-pic">
+                <img class="img8" src="../../../assets/imgs/activity/yinghuaji/title/7.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: "",
+  data () {
+    return {
+        isApp: this.$route.query.platform
+    };
+  },
+  mounted(){
+      window.document.title="樱花季日本美国华盛顿"
+  },
+  methods:{
+      jump(webUrl,appUrl){
+          if(this.isApp){
+              window.location.href=appUrl;
+          }else{
+              this.$router.push({
+                  path:webUrl
+              })
+          }
+      }
+  }
+}
+</script>
+<style lang="scss" scoped>
+    .section1{
+        width:100%;
+        height:1024px;
+        background:url('../../../assets/imgs/activity/yinghuaji/bg/1.jpg') no-repeat center center/750px 1024px;
+    }
+    .section2{
+        width:100%;
+        height:7295px;
+        text-align:center;
+        background:url('../../../assets/imgs/activity/yinghuaji/bg/2.jpg') no-repeat center center/750px 7295px;
+        .s-title{
+            margin:0 auto;
+            padding-top:00px;
+        }
+        .coupon-size{
+            width:750px;
+            height:438px;
+        }
+        .s-pic{
+            text-align: center;
+            margin:0 auto;
+            width:100%;
+            .img1{
+                width:740px;
+                height:612px;
+            }
+            .img2{
+                width:750px;
+                height:184px;
+            }
+            .img3{
+                width:693px;
+                height:321px;
+            }
+            .img4{
+                width:188px;
+                height:60px;
+            }
+            .img5{
+                width:750px;
+                height:293px;
+            }
+            .img6{
+                width:750px;
+                height:206px;
+            }
+            .img7{
+                width:750px;
+                height:142px;
+            }
+            .img8{
+                width:750px;
+                height:226px;
+            }
+        }
+        .pro{
+            text-align: center;
+            .img-large{
+                width:693px;
+                height:333px;
+            }
+        }
+        .pro-flex{
+            width:100%;
+            margin:10px;
+            display:flex;
+            display: -webkit-flex; /* Safari */
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            justify-content: center;
+
+            a{
+                img{
+                    margin-right:15px;
+                    width:340px;
+                    height:260px;
+                }
+            }
+        }
+        .pro-flex2{
+            width:100%;
+            margin:10px;
+            display:flex;
+            display: -webkit-flex; /* Safari */
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            justify-content: center;
+
+            a{
+                img{
+                    margin-right:15px;
+                    width:340px;
+                    height:160px;
+                }
+            }
+        }
+    }
+    
+</style>
