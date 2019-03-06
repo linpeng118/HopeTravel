36b4c60fc6e9f0649e29978ed253cd968c1db05b
于雪花 <yuxuehua@tourscool.com>
2019-03-06 20:17:10 +0800

    报错修改

---
+++ layouts/productList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>sdfsdfsdfsdffffffffffffffffffffffffffffffffffffffff</div>
+    <nuxt keep-alive />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'productList'
+}
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+</style>
---
+++ pages/product_list.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nuxt-child keep-alive :keep-alive-props="{ exclude: ['modal'] }" />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'productList'
+}
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+</style>
