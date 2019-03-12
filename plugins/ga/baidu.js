c6cf9c82a223764826c682ce99cccbbd65272ebb
c_illusion <670199384@qq.com>
2019-03-12 15:15:40 +0800

    测试百度统计

--- plugins/ga/baidu.js
+++ plugins/ga/baidu.js
@@ -1,14 +1,28 @@
+if (process.client && process.env.NODE_ENV === 'production') {
+  var _hmt = _hmt || [];
+  (function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?9bfbbc9f24159633a14d3b4f37db769b";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+}
+
 export default ({
   app: {
     router
   },
   store
 }) => {
-  /* 每次路由变更时进行pv统计 */
+  /*
+   ** 每次路由变更时进行pv统计
+   */
   router.afterEach((to, from) => {
     try {
-      window._hmt = window._hmt || []
-      window._hmt.push(['_trackPageview', to.fullPath])
+      if (process.client && process.env.NODE_ENV === 'production') {
+        window._hmt = window._hmt || []
+        window._hmt.push(['_trackPageview', to.fullPath])
+      }
     } catch (e) {}
   })
 }
