7c4fbb73c670d6da8628dc98e819de21755c016e
c_illusion <670199384@qq.com>
2019-03-14 16:41:27 +0800

    修改axios

--- plugins/axios/index.js
+++ plugins/axios/index.js
@@ -52,14 +52,66 @@ export default function ({
 
   // 响应回调
   $axios.onResponse(res => {
-    // console.log('Making response to ssr ' + res)
+    console.log('Making response to ssr ' + res)
   })
 
   // 错误回调
   $axios.onError(error => {
     const code = parseInt(error.response && error.response.status)
-    if (code === 400) {
-      // console.log('400')
-    }
+    errorHandle(code, error);
   })
+}
+
+/**
+ * 请求失败后的错误统一处理
+ * @param {Number} status 请求失败的状态码
+ */
+const errorHandle = (status, other) => {
+  // 状态码判断
+  switch (status) {
+    // 401: 未登录状态，跳转登录页
+    case 401:
+      toLogin();
+      break;
+      // 403 token过期
+      // 清除token并跳转登录页
+    case 403:
+      tip('登录过期，请重新登录');
+      // localStorage.removeItem('token');
+      // store.commit('loginSuccess', null);
+      // setTimeout(() => {
+      //   toLogin();
+      // }, 1000);
+      break;
+      // 404请求不存在
+    case 404:
+      tip('请求的资源不存在');
+      break;
+    default:
+      tip(`错误码：${status}，${other}`);
+  }
+}
+
+/**
+ * 提示函数
+ * 禁止点击蒙层、显示三秒后关闭
+ */
+const tip = msg => {
+  Notify({
+    message: msg,
+    duration: 3000,
+  });
+}
+
+/**
+ * 跳转登录页
+ * 携带当前页面路由，以期在登录页面完成登录后返回当前页面
+ */
+const toLogin = () => {
+  this.$router.replace({
+    path: '/login',
+    query: {
+      redirect: router.currentRoute.fullPath
+    }
+  });
 }
