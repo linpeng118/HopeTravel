595e60401608cc17b74fd5561e298c046766c380
xianmin <xianmin@tourscool.com>
2019-04-09 18:00:32 +0800

    2

--- api/products.js
+++ api/products.js
@@ -21,6 +21,9 @@ export const getProductList = (data) => {
       category: data.category || '',
       order_by: data.order_by || '',
       order: data.order || '',
+    },headers: {
+      'platform': 'mobile ',
+      'site' : 'mobile'
     }
   },{
     headers: {
