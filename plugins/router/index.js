02b339ac28ac0746c1c23aae0200fa6a218e34ed
c_illusion <670199384@qq.com>
2019-04-10 18:40:55 +0800

    style报错处理

--- plugins/router/index.js
+++ plugins/router/index.js
@@ -8,18 +8,24 @@ export default ({
 
     // 除了首页，列表,定制，其他页面都不弹
     setTimeout(() => {
-      if (!(to.name === 'index' || to.name === 'product_list')) {
-        document.querySelector('#newBridge').style.display = 'none'
-      } else {
-        document.querySelector('#newBridge').style.display = 'block'
+      let newBridgeDom = document.querySelector('#newBridge')
+      if (!newBridgeDom) {
+        return
       }
-    },3000)
-    if(to.name.indexOf('custom') >= 0) {
+      if (!(to.name === 'index' || to.name === 'product_list')) {
+        newBridgeDom.style.display = 'none'
+      } else {
+        newBridgeDom.style.display = 'block'
+      }
+    }, 3000)
+    if (to.name.indexOf('custom') >= 0) {
       var _hmt = _hmt || [];
-      (function() { var hm = document.createElement("script");
+      (function () {
+        var hm = document.createElement("script");
         hm.src = "https://hm.baidu.com/hm.js?72a266736d8b5b47605e2d2ad18f0756";
         var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s); })();
+        s.parentNode.insertBefore(hm, s);
+      })();
     }
     NProgress.start()
     if (to.path.indexOf('.html') >= 0) {}
