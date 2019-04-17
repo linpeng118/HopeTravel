2069032a8a886f3b0320e3298d9030481a95a9ee
c_illusion <670199384@qq.com>
2019-04-17 18:47:31 +0800

    使用官方防护四

--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -2,14 +2,16 @@
 import createPersistedState from 'vuex-persistedstate'
 
 export default ({store}) => {
-  createPersistedState({
-    key: 'tourscool_vuex',
-    reducer(state) {
-      return {
-        // 本地化存储的store数据
-        // profile: state.profile,
-        reservePro: state.reservePro,
+  window.onNuxtReady(() => {
+    createPersistedState({
+      key: 'tourscool_vuex',
+      reducer(state) {
+        return {
+          // 本地化存储的store数据
+          profile: state.profile.profile,
+          reservePro: state.reservePro,
+        }
       }
-    }
-  })(store)
+    })(store)
+  })
 }
