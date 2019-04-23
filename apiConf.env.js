2f836b34d9442b2f705024a2f8e0681d017008b4
xianmin <xianmin@tourscool.com>
2019-04-23 15:23:20 +0800

    放开api配置

---
+++ apiConf.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  base: 'http://api.qa.tourscool.net',
+  payment: 'http://www.htw.tourscool.net',
+  port: 3005,
+  dsnSentry: 'http://d2d1e507e07f4946b59474df0eb5ea30@sentry.tourscool.net:9000/2'
+}
