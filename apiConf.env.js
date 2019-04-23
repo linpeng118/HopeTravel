7a64ea5e7632115575103ca4adf4f7d95f5b0aaa
xianmin <xianmin@tourscool.com>
2019-04-23 16:09:46 +0800

    修改端口

--- apiConf.env.js
+++ apiConf.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   base: 'http://api.qa.tourscool.net',
   payment: 'http://www.htw.tourscool.net',
-  port: 3005,
+  port: 3080,
   dsnSentry: 'http://d2d1e507e07f4946b59474df0eb5ea30@sentry.tourscool.net:9000/2'
 }
