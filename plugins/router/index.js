976b1ee2e175657fb825c9bebe0aa111709ddd94
c_illusion <670199384@qq.com>
2019-04-29 18:29:19 +0800

    hidDom测试

--- plugins/router/index.js
+++ plugins/router/index.js
@@ -1,37 +1,42 @@
 import NProgress from 'nprogress'
 import '@/assets/style/nprogress.css'
+
+// 获取dom
+function getDom(dom) {
+  return document.querySelector(dom) || null
+}
+
 export default ({
   app
 }) => {
   app.router.beforeEach((to, from, next) => {
     let that = this
-    // 获取dom
-    function getDom(dom) {
-      return document.querySelector(dom) || null
-    }
-    // 隐藏dom
-    function hideDom(dom) {
-      let timer
-      // console.log(1, dom);
-      timer = setTimeout((dom) => {
-        // console.log(2, dom);
-        const hideDom = getDom(dom)
-        if (hideDom) {
-          console.log('hideDom', hideDom);
-          if (!(to.name === 'index' || to.name === 'product_list')) {
-            console.log('#newBridge hide');
-            hideDom.style.display = 'none'
-          } else {
-            console.log('#newBridge show');
-            hideDom.style.display = 'block'
-          }
-        } else {
-          clearInterval(timer)
-          that.hideDom(dom)
-        }
-      }, 4000, dom)
-    }
+
     try {
+      // 隐藏dom
+      function hideDom(dom) {
+        let timer
+        // console.log(1, dom);
+        timer = setTimeout((dom) => {
+          // console.log(2, dom);
+          const hideDom = getDom(dom)
+          if (hideDom) {
+            console.log('hideDom', hideDom);
+            if (!(to.name === 'index' || to.name === 'product_list')) {
+              console.log('#newBridge hide');
+              hideDom.style.display = 'none'
+            } else {
+              console.log('#newBridge show');
+              hideDom.style.display = 'block'
+            }
+          } else {
+            clearInterval(timer)
+            that.hideDom(dom)
+          }
+        }, 3000, dom)
+      }
+
+      // 隐藏百度商桥
       hideDom('#newBridge')
 
       NProgress.start()
