1a5dfd91b0ba1a3420ce4f6d2232c398f436e00b
c_illusion <670199384@qq.com>
2019-04-30 10:46:10 +0800

    上传配置文件

---
+++ apiConf.env.js
@@ -0,0 +1,7 @@
+module.exports = {
+  base: 'http://api.qa.tourscool.net',
+  payment: 'http://www.htw.tourscool.net',
+  union: 'http://192.168.1.190:9001',
+  port: 3080,
+  dsnSentry: 'http://d2d1e507e07f4946b59474df0eb5ea30@sentry.tourscool.net:9000/2'
+}
