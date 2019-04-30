6069330bb97f9b21f3d3695b4f9a028697aaa45c
c_illusion <670199384@qq.com>
2019-04-30 12:26:37 +0800

    全局和visa页面ok

---
+++ assets/js/customerService.js
@@ -0,0 +1,39 @@
+/**
+ * 客服系统;分为3个连接：
+ * 全局: 
+ *   https://tb.53kf.com/code/code/10181581/2
+ *   短链接：https://tb.53kf.com/code/client/10181581/2
+ * 定制页面:
+ *   https://tb.53kf.com/code/code/10181581/3
+ *   短链接：https://tb.53kf.com/code/client/10181581/3
+ * 签证页面:
+ *   https://tb.53kf.com/code/code/10181581/4
+ *   短链接：https://tb.53kf.com/code/client/10181581/4
+ */
+export default function (page) {
+  if (page === 'visa') {
+    // 签证页面
+    if (process.env.customerService === "baidu") {
+      window.location.href = 'http://p.qiao.baidu.com/cps/chat?siteId=13206734&userId=26301226'
+    }
+    if (process.env.customerService === "53kf") {
+      window.location.href = 'https://tb.53kf.com/code/client/10181581/4'
+    }
+  } else if (page === 'custom') {
+    // 定制页面
+    if (process.env.customerService === "baidu") {
+      window.location.href = 'http://p.qiao.baidu.com/cps/chat?siteId=12918104&userId=26301226'
+    }
+    if (process.env.customerService === "53kf") {
+      window.location.href = 'https://tb.53kf.com/code/client/10181581/3'
+    }
+  } else {
+    // 全局
+    if (process.env.customerService === "baidu") {
+      window.location.href = 'http://p.qiao.baidu.com/cps/chat?siteId=12524949&userId=26301226'
+    }
+    if (process.env.customerService === "53kf") {
+      window.location.href = 'https://tb.53kf.com/code/client/10181581/2'
+    }
+  }
+}
