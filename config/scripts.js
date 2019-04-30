b3ed7e44be81622ad403f81d5feb0cddb7119c61
c_illusion <670199384@qq.com>
2019-04-30 11:44:43 +0800

    配置53客服

---
+++ config/scripts.js
@@ -0,0 +1,25 @@
+const apiConfig = require('../apiConf.env.js')
+
+let scriptArr = [{
+  src: '/flexible/flexible.js',
+  type: 'text/javascript',
+  charset: 'utf-8',
+}, {
+  src: '/polyfill/index.js',
+  type: 'text/javascript',
+  charset: 'utf-8',
+},]
+
+if (apiConfig.customerService === '53kf') {
+  scriptArr.push({
+    src: 'https://tb.53kf.com/code/code/10181581/2'
+  })
+}
+
+if (apiConfig.customerService === 'baidu') {
+  scriptArr.push({
+    src: 'https://hm.baidu.com/hm.js?9bfbbc9f24159633a14d3b4f37db769b'
+  })
+}
+
+module.exports = scriptArr
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,21 @@
 const TerserPlugin = require('terser-webpack-plugin')
 const pkg = require('./package')
-const pluginConfig = require('./config/plugins')
 const apiConfig = require('./apiConf.env.js')
+const pluginConfig = require('./config/plugins')
+const scriptConfig = require('./config/scripts')
+
 const axiosUrl = `http://127.0.0.1:${apiConfig.port}`
 console.log('apiConfig:', apiConfig)
 console.log('axiosUrl:', axiosUrl)
 const LRU = require('lru-cache')
+
 
 module.exports = {
   mode: 'universal',
   dev: (process.env.NODE_ENV !== 'production'),
   env: {
     ENV_TYPE: process.env.ENV_TYPE, // 添加一个环境变量
+    customerService: apiConfig.customerService
   },
   /*
    ** Headers of the page
@@ -52,18 +56,8 @@ module.exports = {
       rel: 'icon',
       type: 'image/x-icon',
       href: '/favicon.ico',
-    }, ],
-    script: [{
-      src: '/flexible/flexible.js',
-      type: 'text/javascript',
-      charset: 'utf-8',
-    }, {
-      src: '/polyfill/index.js',
-      type: 'text/javascript',
-      charset: 'utf-8',
-    }, {
-      src: 'https://hm.baidu.com/hm.js?9bfbbc9f24159633a14d3b4f37db769b'
-    }],
+    },],
+    script: scriptConfig,
     __dangerouslyDisableSanitizers: ['script'],
   },
 
@@ -167,8 +161,8 @@ module.exports = {
     // https://github.com/nuxt/nuxt.js/pull/4600
     babel: {
       presets({
-                isServer
-              }) {
+        isServer
+      }) {
         return [
           [
             "@nuxt/babel-preset-app",
@@ -176,8 +170,8 @@ module.exports = {
               targets: isServer ? {
                 node: "current"
               } : {
-                browsers: ["ie >= 10"],
-              }
+                  browsers: ["ie >= 10"],
+                }
             },
             "es2015",
           ]
