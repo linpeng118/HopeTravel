0e684de9ce0a783782625194b848675b3cc6bf37
c_illusion <670199384@qq.com>
2019-05-20 13:55:00 +0800

    header头修改

--- api/confirm_order.js
+++ api/confirm_order.js
@@ -1,46 +1,38 @@
 import axios from '@/plugins/axios/axios'
 
 export const countprice = (data) => {
-  return axios.post(`/api/product/${data.product_id}/budget?time=`+new Date().getTime(), {
+  return axios.post(`/api/product/${data.product_id}/budget?time=` + new Date().getTime(), {
     product: {
-      product_id: data.product_id||'',
-      departure_date: data.departure_date||'',
-      room_total: data.room_total||0,
-      room_attributes: data.room_attributes||[],
-      product_departure: data.product_departure||'',
-      attributes: data.attributes||[],
-      is_point: data.is_point||false,
-      coupon_cus_id:data.coupon_cus_id||'',
+      product_id: data.product_id || '',
+      departure_date: data.departure_date || '',
+      room_total: data.room_total || 0,
+      room_attributes: data.room_attributes || [],
+      product_departure: data.product_departure || '',
+      attributes: data.attributes || [],
+      is_point: data.is_point || false,
+      coupon_cus_id: data.coupon_cus_id || '',
     }
-  },{
-    headers: { 'platform': 'm',
-      'site' : 'm'}
   })
 }
-export const addorder = (data,referrerId,channel) => {
+export const addorder = (data, referrerId, channel) => {
   // return axios.post(`/api/order`, data)
   return axios({
     method: 'post',
     url: `/api/order`,
     data,
-    headers: {
-      'Referrer-Id': referrerId || 0,
-      'Channel': channel || 'm',
-      'platform': 'm ',
-      'site': 'm'
-    }
+    // headers: {
+    //   'Referrer-Id': referrerId || 0,
+    //   'Channel': channel || 'm',
+    //   'platform': 'm ',
+    // }
   })
 }
 //获取我的可用优惠卷
 export const orderCouponList = (data) => {
-  return axios.get(`/api/product/${data.product_id}/usablecoupon`,{
+  return axios.get(`/api/product/${data.product_id}/usablecoupon`, {
     params: {
       departure: data.departure,
-      price:data.price,
-    },
-    headers: {
-      'platform': 'm ',
-      'site': 'm'
+      price: data.price,
     }
-  },)
+  })
 }
