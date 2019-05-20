63b81148fe372e404d6a3185a917639718e880f1
c_illusion <670199384@qq.com>
2019-05-20 17:32:41 +0800

    还原接口地址

--- server/index.js
+++ server/index.js
@@ -17,8 +17,8 @@ async function start() {
   const nuxt = new Nuxt(config)
 
   const {
-    host = process.env.HOST || '192.168.1.226',
-    // host = process.env.HOST || '127.0.0.1',
+    // host = process.env.HOST || '192.168.1.226',
+    host = process.env.HOST || '127.0.0.1',
     port = process.env.PORT || 3000 
 
   } = nuxt.options.server
