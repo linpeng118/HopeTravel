c80c8197dd393a0e5bd71e41d03ce54b56ab8b16
xianmin <xianmin@tourscool.com>
2019-07-09 11:28:48 +0800

    定制页修改

--- pages/custom/index.vue
+++ pages/custom/index.vue
@@ -66,7 +66,7 @@
         </div>
       </div>
       <!-- 客人评价 -->
-      <div class="season-recommend" ref="pingjia">
+      <div class="season-recommend" ref="pingjia" v-if="showpingjia">
         <div class="season-title">客人评价</div>
         <div class="season-wrap swiper-container" v-swiper:mySwiper="swiperOption">
           <div class="swiper-wrapper setsw">
@@ -85,7 +85,7 @@
         </div>
       </div>
       <!-- 经典路线 -->
-      <div class="season-recommend" style=" height: auto!important;" ref="luxian">
+      <div class="season-recommend" style=" height: auto!important;" ref="luxian" v-if="showpingjia2">
         <div class="season-title">经典路线</div>
         <div
           class="season-item2"
