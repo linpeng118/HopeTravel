3564c09058d647891d979a239f14f69c06aa054d
xianmin <xianmin@tourscool.com>
2019-08-06 17:06:14 +0800

    2

---
+++ pages/attack/detail.vue
@@ -0,0 +1,551 @@
+<template>
+  <div>
+  <div class="banner">
+    <a v-if="!isApp" class="return" @click="goBack" href="javascript:;"></a>
+    <a v-if="!isApp" class="right" @click="goBack" href="javascript:;"></a>
+    <img v-if="objdata&&objdata.cover" :src="objdata.cover" alt="">
+  </div>
+    <div class="con" v-if="objcon&&objcon.length">
+      <div class="attack-head" v-if="objdata">
+        <div class="imgbox"><img :src="objdata.create_user.face" alt=""></div>
+        <div class="conbox">
+          <p>
+            <span style="text-align: left">{{objdata.create_user.name||'佚名'}}</span>
+            <span style="text-align: right"><img src="../../assets/imgs/addres.png" alt=""> {{objdata.relate_position}}</span>
+          </p>
+          <p>
+            <span style="text-align: left">{{objdata.create_user.brief}}</span>
+            <span style="text-align: right">阅读{{objdata.read_count}}&nbsp;&nbsp;&nbsp;{{objdata.create_at?objdata.create_at.substr(0,10):''}}</span>
+          </p>
+        </div>
+      </div>
+      <div class="attack-item" v-for="(item,ind) in objcon" :key="ind">
+        <p class="titlex">{{item.chapter_title}}</p>
+        <div class="conx" v-html="item.content">
+        </div>
+        <template v-if="item.relate_product&&item.relate_product.length">
+          <div class="prox" v-for="(pro,inde) in item.relate_product" :key="inde" @click="topro(pro.product_id)">
+           <div class="imgbox">
+             <img :src="pro.image" alt="">
+           </div>
+            <div class="conbox">
+              <p v-html="pro.name.length>29?pro.name.substr(0,26)+'...':pro.name">
+              </p>
+              <p>
+                <span>{{pro.default_price}}</span>&nbsp;<span>起</span>
+              </p>
+            </div>
+          </div>
+        </template>
+
+      </div>
+      <div class="bottom-user">
+        <p class="p1">
+          <img :src="objdata.create_user.face" alt="">
+        </p>
+        <p class="p2">{{objdata.create_user.name||'佚名'}}</p>
+        <p class="p3">{{objdata.create_user.brief}}</p>
+      </div>
+
+    </div>
+    <div class="boxfoot">
+      <input type="text" v-model="searchtext">
+      <label @click="subcomm()">{{$t('submit')}}</label>
+      <span v-if="objdata" @click="firstload()"><img src="../../assets/imgs/ic_comment@2x.png" alt=""><i>{{objdata.comment_count}}</i></span>
+      <span v-if="!isfav&&objdata" @click="addfav()"><img src="../../assets/imgs/ic_like@2x.png" alt=""><i>{{objdata.collection_count}}</i></span>
+      <span v-else-if="objdata" @click="delfav()"><img src="../../assets/imgs/ic_like2@2x.png" alt=""><i>{{objdata.collection_count}}</i></span>
+      <span v-if="objdata"><img src="../../assets/imgs/ic_share@2x.png" alt=""><i>{{objdata.share_count}}</i></span>
+    </div>
+    <van-popup
+      v-model="showcomm"
+      round
+      position="bottom"
+      class="popup"
+    >
+    <div class="comment">
+      <van-nav-bar :title="$t('attackcomm')" @click-right="showcomm=false" >
+        <van-icon name="cross" slot="right"/>
+      </van-nav-bar>
+      <p class="elsep" v-if="objdata">{{$t('allcomm')}}{{objdata.comment_count}}条</p>
+
+      <van-pull-refresh v-model="isLoading" @refresh="onRefresh" v-if="datacomm.length>0">
+        <van-list v-model="isLoading" :finished="prodFinished" :finished-text="$t('noMore')" @load="onLoad" :immediate-check="false">
+          <template v-if="datacomm.length">
+            <van-cell v-for="(item,ind) in datacomm" :key="ind">
+              <div class="itemcom">
+                <p>{{item.user_name}} <span v-html="item.created.substr(0,10)"></span></p>
+                <p v-html="item.content"></p>
+              </div>
+              <img :src="item.face" alt="">
+            </van-cell>
+          </template>
+        </van-list>
+      </van-pull-refresh>
+      <p class="submitp">
+        <input type="text" v-model="searchtext">
+        <label @click="subcomm()">{{$t('submit')}}</label>
+      </p>
+    </div>
+    </van-popup>
+
+
+</div>
+</template>
+<script>
+  import {getattackobj,getattackobj2,getattaccomm,upcomm,getisfa,addFavorite2,delFavorite2} from '@/api/products'
+  export default {
+
+    name: "attack-detail",
+    data() {
+      return {
+        isApp: this.$route.query.platform,
+        objid: this.$route.query.attackId,
+        objdata:null,
+        objcon:null,
+        showcomm:false,//是否显示评论列表
+        datacomm:[],
+        prodFinished:false,
+        searchtext:"",
+        isfav:0,
+      };
+    },
+    mounted() {
+      this.getpro()
+      this.getisfav()
+    },
+    methods: {
+      goBack() {
+        window.history.go(-1);
+      },
+      //第一次获取评论
+      async firstload(){
+        // 获取数据
+        let that=this;
+        that.datacomm=[]
+        let {code,pagination,data} = await getattaccomm({
+          page:1,
+          id:that.objid
+        })
+        if (code === 0) {
+          that.datacomm=data
+          that.pagination=pagination;
+          that.isLoading = false
+          that.showcomm=true
+          // 数据全部加载完成
+          if (that.pagination.total_page==that.pagination.page) {
+            that.prodFinished = true
+
+          }
+
+        }
+      },
+      async getpro() {
+        this.objcon=null;
+        let {code, data} = await getattackobj2(this.objid)
+        if (code === 0) {
+          this.objdata=data.base;
+          this.objcon=data.chapters;
+        }
+      },
+      // 是否收藏
+      async getisfav() {
+        let {code, data} = await getisfa(this.objid)
+        if (code === 0) {
+          this.isfav=data.is_favorite;
+        }
+      },
+      async addfav() {
+        let {code} = await addFavorite2(this.objid)
+        if (code === 0) {
+          this.isfav=1;
+        }
+      },
+
+      async delfav() {
+        let {code,data} = await delFavorite2(this.objid)
+        if (code === 0) {
+          this.isfav=0;
+        }
+      },
+
+      topro(productId) {
+        this.$router.push({
+          name: 'product-detail',
+          query: {
+            productId
+          }
+        });
+      },
+      // 上拉刷新
+      async onRefresh(){
+        // 获取数据
+        let that=this;
+        let {code, data} = await getattaccomm({
+          page:that.pagination.page-1||1,
+          id:that.objid
+        })
+        if (code === 0) {
+          that.datacomm.push(...data.items)
+          that.pagination=data.pagination;
+          that.isLoading = false
+          // 数据全部加载完成
+          if (that.pagination.total_page==that.pagination.page) {
+            that.prodFinished = true
+          }
+
+        }
+
+      },
+      // 滑动会请求数据
+      async onLoad() {
+        // 获取数据
+        let that=this;
+        let {code, data } = await upcomm({
+          page:that.pagination.page+1||1,
+          id:that.objid
+        })
+        if (code === 0) {
+          that.datacomm.push(...data.items)
+          that.pagination=data.pagination;
+          that.isLoading = false
+          // 数据全部加载完成
+          if (that.pagination.total_page==that.pagination.page) {
+            that.prodFinished = true
+          }
+        }
+      },
+      //提交评论
+      async subcomm(){
+        // 获取数据
+        let that=this;
+        let {code} = await getattaccomm({
+          comment:that.searchtext,
+          id:that.objid
+        })
+        if (code === 0) {
+          that.searchtext=''
+          that.$dialog.alert({
+            message: '评论成功'
+          });
+        }
+      }
+
+    }
+  };
+</script>
+<style>
+  .attack-item img{
+    max-width: 686px;
+    height: auto;
+  }
+
+</style>
+<style lang="scss" scoped>
+  @import '../../assets/style/custom/index';
+
+  .banner {
+    width: 750px;
+    height: 520px;
+    position: relative;
+    .right{
+      position: absolute;
+      top: 20px;
+      right: 0;
+      display: inline-block;
+      width: 100px;
+      height: 100px;
+      background: url('~assets/imgs/custom/more.png') no-repeat center center/40px 42px;
+    }
+    img{
+      width: 750px;
+      height: 520px;
+      display: inline-block;
+      background-color: #8f8f8f;
+    }
+    }
+  .con{
+    padding: 32px;
+    margin-bottom:88px;
+    width: 750px;
+    box-sizing: border-box;
+    border-radius: 30px;
+    margin-top: -40px;
+    position: relative;
+    background-color: #fff;
+    z-index: 22;
+  }
+  .attack-head{
+    height: 80px;
+    .imgbox{
+      width: 80px;
+      height: 80px;
+      float: left;
+      img{
+        width: 80px;
+        height: 80px;
+        display: inline-block;
+        background-color: #ccc;
+      }
+
+    }
+    .conbox{
+      width: 580px;
+      height: 80px;
+      float: right;
+      line-height: 4;
+      p:nth-child(1){
+        font-size: 24px;
+        line-height:40px;
+        color: #a2a2a2;
+        span{
+          display: inline-block;
+          width: 65%;
+        }
+        span:nth-child(1){
+          color: #2d2d2d;
+          font-size: 28px;
+          width: 30%;
+        }
+        img{
+          width: 16px;
+          height: 21px;
+        }
+      }
+      p:nth-child(2){
+        span{
+          display: inline-block;
+          width: 47.7%;
+        }
+
+        font-size: 24px;
+        line-height:40px;
+        color: #a2a2a2;
+
+      }
+
+    }
+  }
+  .attack-item{
+    width:100%;
+
+    .titlex{
+      font-size: 40px;
+      line-height: 60px;
+      margin: 20px 0;
+      color: #2d2d2d;
+    }
+    .conx{
+      font-size: 32px;
+      color: #707070;
+      text-align: left;
+      word-wrap:break-word;
+      img{
+        max-width: 686px;
+        height: auto;
+      }
+      p{
+        img{
+          max-width: 686px;
+          height: auto;
+        }
+      }
+    }
+    .prox{
+      width: 100%;
+      height: 200px;
+      border-radius: 20px;
+      overflow: hidden;
+      box-shadow:0px 0px 28px rgba(52,52,52,0.2);
+      margin: 20px 0;
+      .imgbox{
+        width:220px;
+        height: 200px;
+        float: left;
+        img{
+          width: 220px;
+          height: 200px;
+          display: inline-block;
+          background-color: #ccc;
+        }
+
+      }
+      .conbox{
+        width: 426px;
+        float: right;
+        height: 200px;
+        color: #2d2d2d;
+        font-size: 28px;
+        padding: 20px 5px;
+        p:nth-child(2){
+          margin-top: 20px;
+          span:nth-child(1){
+            font-size: 44px;
+            color:#f15b4a;
+          }
+          span:nth-child(2){
+            font-size: 20px;
+            color:#b1b1b1;
+
+          }
+        }
+      }
+    }
+
+
+  }
+  .boxfoot{
+    width:750px;
+    height:88px;
+    background-color: #fff;
+    border-top: 1px solid rgba(236,236,236,1);
+    position: fixed!important;
+    bottom: 0;
+    opacity:1;
+    z-index: 200;
+    padding: 16px 32px;
+    label{
+      font-size: 24px;
+      color: #4bb1f5;
+      padding: 10px;
+      position: relative;
+      top:-18px;
+    }
+    input{
+      width:360px;
+      height:56px;
+      background:rgba(241,241,241,1);
+      opacity:1;
+      border-radius:330px;
+      position: relative;
+      top:-18px;
+      font-size: 24px;
+      padding: 0 8px;
+      border: none;
+    }
+    span{
+
+      img{
+        width:57px;
+        height: 57px;
+      }
+      margin-left: 8px;
+      i{
+         font-size: 18px;
+         color:#399ef6;
+        position: absolute;
+        font-style: normal;
+      }
+    }
+    span:nth-child(1){
+      margin-left: 34px;
+      img{
+        width: 61px;
+        height: 57px;
+      }
+    }
+  }
+  .popup{
+    width: 100%;
+    height: 1200px;
+    background-color: rgba(0,0,0,0);
+  }
+  .comment{
+    border-radius: 30px 30px 0 0;
+    overflow: hidden;
+    background-color: #fff;
+    .elsep{
+      font-size: 28px;
+      color: #d9d9d9;
+      padding-left: 30px;
+    }
+    .submitp{
+      width: 750px;
+      background-color: #fff;
+      padding: 32px 16px;
+      border-top: 1px solid #dfdfdf;
+      position: fixed;
+      bottom: 0;
+      label{
+        font-size: 24px;
+        color: #4bb1f5;
+        padding: 10px;
+        position: relative;
+      }
+      input{
+        width:640px;
+        height:56px;
+        background:rgba(241,241,241,1);
+        opacity:1;
+        border-radius:330px;
+        position: relative;
+        font-size: 24px;
+        padding: 0 8px;
+        border: none;
+      }
+    }
+    .van-nav-bar{
+      box-shadow: none!important;
+    }
+    .van-list{
+      height:982px;
+      overflow-y: scroll;
+      padding-bottom: 80px;
+    }
+    .itemcom{
+      width: 600px;
+      float: right;
+      min-height:146px ;
+      p:nth-child(1){
+        color: #2d2d2d;
+        font-size: 28px;
+        margin-bottom: 10px;
+        span{
+          font-size: 20px;
+          color: #d9d9d9;
+        }
+      }
+    }
+    img{
+      width: 60px;
+      height: 60px;
+      display: inline-block;
+      background-color: #f2f2f2;
+      position: absolute;
+      margin-top: 43px;
+    }
+    .mylist{
+      .van-cell{
+        background-color: rgba(0,0,0,0)!important;
+      }
+    }
+    .bottom-user{
+      text-align: center;
+      line-height: 50px;
+      width: 100%;
+      padding: 40px 0;
+      .p1{
+        text-align: center;
+        img{
+          width: 80px;
+          height: 80px;
+          border-radius: 80px;
+          display: inline-block;
+          background-color: #ccc;
+        }
+
+      }
+      .p2{
+        text-align: center;
+        font-size: 28px;
+        color: #2d2d2d;
+      }
+      .p3{
+        text-align: center;
+        font-size: 24px;
+        color: #ccc;
+      }
+    }
+
+  }
+
+</style>
---
+++ pages/attack/list.vue
@@ -0,0 +1,397 @@
+<template>
+  <div>
+    <van-nav-bar class="layout-header tours-no-bb classBg show-bg"
+                 ref="layoutHeader"
+                 fixed
+                 :z-index="999"
+                 @click-left="leftClick">
+      <van-icon class="left-wrap"
+                name="arrow-left"
+                slot="left"/>
+      <van-icon class="right-wrap" slot="right">
+        <div class="big-search">
+          <div :class="isSearch ? 'has-keyword': ''">
+            <div class="search-box" >
+              <van-icon name="search" />
+              <input class="box" show-action v-model="query1" @search="getpro" />
+            </div>
+            <div class="search-btn" @click="getpro" v-if="isSearch">{{$t('headerSearch.search')}}</div>
+          </div>
+        </div>
+      </van-icon>
+    </van-nav-bar>
+    <div class="product-list">
+      <van-pull-refresh v-model="isLoading" @refresh="onRefresh" v-if="datax.length>0">
+        <van-list v-model="isLoading" :finished="prodFinished" :finished-text="$t('noMore')" @load="onLoad" :immediate-check="false">
+          <template v-if="datax.length">
+            <van-cell v-for="(data,ind) in datax" :key="ind">
+              <div class="product-item" @click="selectDetail(data.article_id)" target="_blank">
+                <div class="box-img">
+                  <img :src="data.cover" alt="">
+                </div>
+                <div class="con">
+                  <div class="p1">{{data.name}}</div>
+                  <p class="p2">
+                    <img :src="data.create_user.face" alt="">
+                    <span style="color:#575757;">{{data.create_user.name||'佚名'}}</span>
+                    <span>阅读{{data.read_count}}</span>
+                    <span>评论{{data.comment_count}}</span>
+                  </p>
+                  <p class="p3">
+                    <span>{{data.create_at}}</span>
+                    <span v-if="data.relate_position!=''">
+                      <img src="../../assets/imgs/addres.png" alt="">
+                      <a v-html="data.relate_position.length>7?data.relate_position.substr(0,5)+'...':data.relate_position"></a>
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </van-cell>
+          </template>
+          <!--<div v-else>暂无数据</div>-->
+        </van-list>
+      </van-pull-refresh>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import {getattack} from '@/api/products'
+
+  export default {
+    name: 'productList',
+    filters: {
+      showInt(val) {
+        if(val){
+          return val.split('.')[0]
+        }
+      },
+    },
+    data(){
+      return {
+        isShowFx: false,
+        isLoading: false,
+        datax:[],
+        pagination:{
+          page:1,
+        },
+        objid:this.$route.query.id||202,
+        prodFinished:false,
+        showTag:false,
+        query1:'',//关键字
+        isSearch:false,
+      }
+    },
+    computed: {
+      iconTour() {
+        return this.data.icons_tour
+      },
+    },
+    watch:{
+      'query1':function (val,oldval) {
+        if(val!=''){
+          this. isSearch=true
+        }
+        else{
+          this. isSearch=false
+        }
+      }
+    },
+    mounted(){
+      this.getpro()
+    },
+    methods: {
+      selectDetail(attackId) {
+        this.$router.push({
+          path: '/attack/detail',
+          query: {
+            'attackId':attackId
+          }
+        });
+      },
+      async getpro() {
+        this.datax=[];
+        let {code, data} = await getattack({
+          page:1,
+          keyword:this.query1
+        })
+        if (code === 0) {
+          this.datax=data.items;
+          this.pagination=data.pagination;
+        }
+
+      },
+      // 滑动会请求数据
+      async onLoad() {
+        // 获取数据
+        let that=this;
+        let {code, data } = await getattack({
+          page:that.pagination.page+1,
+          keyword:that.query1
+        })
+        if (code === 0) {
+          that.datax.push(...data.items)
+          that.pagination=data.pagination;
+          that.isLoading = false
+          // 数据全部加载完成
+          if (that.pagination.total_page==that.pagination.page) {
+            that.prodFinished = true
+          }
+        }
+      },
+      // 返回上一级
+      leftClick() {
+        this.$router.go(-1)
+
+      },
+      // 上拉刷新
+      async onRefresh(){
+        // 获取数据
+        let that=this;
+        that.datax=[];
+        let {code, data} = await getattack({
+          page:1,
+          coupon_id:this.objid
+        })
+        if (code === 0) {
+          this.datax=data.items;
+          this.pagination=data.pagination;
+          that.isLoading = false
+          // 数据全部加载完成
+          if (that.pagination.total_page==that.pagination.page) {
+            that.prodFinished = true
+          }
+
+        }
+
+      },
+    }
+  }
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+  .layout-header {
+    height: 88px;
+    font-size: 32px;
+    color: #eee;
+    background-color: transparent;
+    transition: all 0.5s;
+    overflow: hidden;
+    &.show-bg {
+      background-color: #fff;
+      color: #191919;
+      box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.14);
+      .left-wrap {
+        /*color: #191919;*/
+      }
+    }
+    .right-wrap {
+      .big-search{
+        width: 634px;
+        height: 72px;
+      }
+      .van-search{
+        background: #fff !important;
+        border-radius:36px;
+        height: 72px;
+        padding: 0;
+      }
+    }
+  }
+  .search-box{
+    display: flex;
+    display: -webkit-flex;
+    align-items: center;
+    -webkit-align-items: center;
+    box-sizing: border-box;
+    width:618px;
+    padding: 0 6px 0 20px;
+    height:68px;
+    background: #DBDBDB;
+    border-radius:36px;
+    overflow: hidden;
+    i.van-icon-search{
+      color: #fff;
+      font-size: 40px;
+    }
+    .box{
+      width: 550px;
+      border: none;
+      outline: 0;
+      color: #666;
+      background: #DBDBDB;
+      padding: 10px 0;
+      line-height: 40px;
+      &.list{
+        width: 470px !important;
+      }
+    }
+    input{
+
+    }
+    i.van-icon-clear{
+      color: #666;
+      padding-right: 10px;
+    }
+  }
+  .has-keyword{
+    display: flex;
+    display: -webkit-flex;
+    align-items: center;
+    justify-content: center;
+    .search-box{
+      flex: 1;
+      -webkit-flex: 1;
+      align-items: center;
+      .box{
+        width: 460px;
+        padding: 10px 0;
+      }
+    }
+    .search-btn{
+      line-height: 72px;
+      padding-left: 10px;
+      color: #00ABF9;
+    }
+    .personal-btn{
+      font-size: 48px !important;
+      padding-left: 10px;
+      i{
+        vertical-align: bottom;
+        font-size: 48px !important;
+      }
+    }
+  }
+  .product-list{
+    padding-top: 70px;
+    .product-item{
+      width:750px;
+      height:300px ;
+      padding-top: 10px;
+      box-sizing: border-box;
+      .box-img{
+        width: 240px;
+        display: inline-block;
+        height: 240px;
+        border-radius:12px;
+        overflow: hidden;
+        position: absolute;
+        background-color: #4bb1f5;
+        img{
+          width: 240px;
+          height: 240px;
+        }
+      }
+      .con{
+        background-color: #ffd97f;
+        padding:20px 28px 20px 180px ;
+        margin-top: 36px;
+        width: 620px;
+        height: 240px;
+        box-sizing: border-box;
+        background:rgba(255,255,255,1);
+        box-shadow:0px 0px 28px rgba(52,52,52,0.2);
+        border-radius: 12px;
+        margin-left: 60px;
+        .p1{
+          width: 418px;
+          font-size: 28px!important;
+          color: #2d2d2d;
+          line-height: 44px;
+          padding-left: 20px;
+          height: 110px;
+          padding-bottom: 20px;
+        }
+        .p2{
+          width: 418px;
+          height: 48px;
+          font-size: 24px;
+          color: #d4d4d4;
+          line-height: 48px;
+          img{
+            width: 48px;
+            height: 48px;
+            background-color: #cfcfcf;
+            margin-left: 20px;
+            display: inline-block;
+            position: relative;top: 16px;
+          }
+          span{
+            margin-left: 12px;
+          }
+        }
+        .p3{
+          width: 418px;
+          height: 48px;
+          font-size: 24px;
+          text-align: right;
+          color: #d4d4d4;
+          margin-top: 8px;
+          span{
+            margin-left: 20px;
+            line-height: 36px;
+            display: inline-block;
+            img{
+              width: 16px;
+              height: 21px;
+              display: inline-block;
+            }
+          }
+
+
+        }
+      }
+
+    }
+  }
+  .setcop{
+    width:750px;
+    height:88px;
+    background:rgba(254,248,236,1);
+    opacity:1;
+    line-height: 88px;
+    text-align: center;
+    font-size: 24px;
+    color: #FBA35A;
+  }
+  .setspecial{
+    display: inline-block;
+    margin-right: 10px;
+    font-size: 20px;
+    font-weight: 300;
+    color:rgba(251,96,93,1);
+    position: relative;
+    overflow: hidden;
+    padding: 2px 0 0 0 ;
+  }
+  .ileft{
+    display: inline-block;
+    width: 14px;
+    height: 14px;
+    border-radius: 14px;
+    background-color: #fff;
+    position: absolute;
+    border: 2px solid rgba(251,96,93,1);
+    margin-top: 15px;
+    margin-left: -7px;
+  }
+  .iright{
+    display: inline-block;
+    width: 14px;
+    height: 14px;
+    border-radius: 14px;
+    background-color: #fff;
+    position: absolute;
+    border: 2px solid rgba(251,96,93,1);
+    margin-top: -28px;
+    margin-left:calc(100% - 7px);
+  }
+  .icon{
+    border: 2px solid rgba(251,96,93,1);
+    padding: 10px 14px 10px 10px;
+    line-height: 20px;
+    border-radius: 8px;
+    display: inline-block;
+  }
+</style>
