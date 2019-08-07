ff8243e783a3b2bbbfbceecab4175183e73942f1
c_illusion <670199384@qq.com>
2019-08-07 15:11:35 +0800

    修复冲突

--- pages/product_list/product-save.vue
+++ pages/product_list/product-save.vue
@@ -8,57 +8,6 @@
                 name="arrow-left"
                 slot="left" />
     </van-nav-bar>
-<<<<<<< HEAD
-    <div class="product-list">
-      <van-pull-refresh v-model="isLoading" @refresh="onRefresh" v-if="datax.length>0">
-        <van-list v-model="isLoading" :finished="prodFinished" :finished-text="$t('noMore')" @load="onLoad" :immediate-check="false">
-          <template v-if="datax.length">
-            <van-cell v-for="(data,ind) in datax" :key="ind">
-              <div class="product-item" @click="selectDetail(data.product_id)" target="_blank">
-                <div class="img-show">
-                  <img :src="data.image" alt="">
-                  <div class="tags2"v-if="data.is_soldout">
-                    {{$t('saleOver')}}
-                    <p v-if="showTag" >{{productTypeValue(data.product_type)}}</p>
-                  </div>
-                  <div class="tags" v-if="showTag&&!data.is_soldout">{{productTypeValue(data.product_type)}}</div>
-                  <div class="special-icon">
-                    <span v-for="(item,index) in data.special_icons" :class="item.type === 'special' ? 'color': ''" :key="index">{{item.title}}</span>
-                    <!--<span>3折</span>-->
-                  </div>
-                </div>
-                <div class="product-desc">
-                  <div class="title">
-                    {{data.name}}
-                  </div>
-                  <div class="tags-wrap">
-                    <span class="solid" v-if="data.self_support">{{$t('selfSupport')}}</span>
-                    <span v-for="item in data.icons_show" :key="item" class="hollow color">{{item}}</span>
-                    <span v-for="(item,index) in data.icons_tour" :key="index" class="hollow">
-            <!--<template v-if="index < 2">{{item.title}}</template>-->
-            {{item.title}}
-          </span>
-                  </div>
-                  <div class="product-price">
-                    <template v-if="!data.special_price">
-                      <span class="sale-price"><strong>{{data.default_price | showInt}}</strong>/{{$t('since')}}</span>
-                    </template>
-                    <template v-else>
-                      <span class="sale-price"><strong>{{data.special_price | showInt}}</strong>/{{$t('since')}}</span><span class="default-price" style="text-decoration: line-through">{{$t('listComp.originalPrice')}}：{{data.default_price | showInt}}</span>
-                    </template>
-                    <span v-if="data.coupons&&data.coupons.length" class="setspecial">
-               <i class="ileft"></i>
-               <i class="icon">{{data.coupons[0]}}</i>
-               <i class="iright"></i>
-          </span>
-                    <span v-if="data.coupons&&data.coupons.length>1" style="color:#fb605d">......</span>
-                    <span class="share-p" v-if="isShowFx">{{$t('productDetailPage.shareMakes')}}{{data.agent_fee}}</span>
-                    <p><span style="color: #989898" v-if="data.comment_score">{{data.comment_score}}分</span>&nbsp;
-                      <span style="color: #989898">{{data.sales}}人出行</span></p>
-                  </div>
-                </div>
-              </div>
-=======
     <div class="product-list product-listx">
       <van-pull-refresh v-model="isLoading" @refresh="onRefresh" v-if="datax.length>0">
         <van-list v-model="isLoading" :finished="prodFinished" :finished-text="$t('noMore')" @load="onLoad" :immediate-check="false">
@@ -67,19 +16,12 @@
               <div class="count-down"
                    v-html="datak.time"></div>
               <saveitem :datay="datak"></saveitem>
->>>>>>> qa
             </van-cell>
           </template>
           <!--<div v-else>暂无数据</div>-->
         </van-list>
 
 
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> qa
       </van-pull-refresh>
 
     </div>
@@ -89,17 +31,12 @@
 
 <script>
   import {getProductCoup} from '@/api/products'
-<<<<<<< HEAD
-  export default {
-    name: 'productList',
-=======
   import saveitem from '@/components/items/saveitem'
   export default {
     name: 'productList',
     components: {
       saveitem
     },
->>>>>>> qa
     filters: {
       showInt(val) {
         if(val){
@@ -144,11 +81,7 @@
       },
     },
     mounted(){
-<<<<<<< HEAD
-      this.getpro()
-=======
       this.getTime()
->>>>>>> qa
       this.datax = this.indexData[2].data
     },
     methods: {
@@ -175,41 +108,6 @@
           }
         });
       },
-<<<<<<< HEAD
-      async getpro(id) {
-        this.datax=[];
-        let {code, data, coupons ,pagination} = await getProductCoup({
-          page:1,
-          coupon_id:this.objid
-        })
-        if (code === 0) {
-          this.datax=data;
-          this.coupons=coupons;
-          this.pagination=pagination;
-        }
-
-      },
-
-      // 滑动会请求数据
-      async onLoad() {
-        // 获取数据
-        let that=this;
-        let {code, data, coupons ,pagination} = await getProductCoup({
-          page:that.pagination.page+1,
-          coupon_id:this.objid
-        })
-        if (code === 0) {
-          that.datax=data;
-          that.datax.push(...data)
-          that.coupons=coupons;
-          that.pagination=pagination; that.isLoading = false
-          // 数据全部加载完成
-          if (!that.pagination.more) {
-            that.prodFinished = true
-          }
-
-        }
-=======
       // 倒计时时间转化
       timeToData(maxtime) {
         let second = Math.floor(maxtime % 60);       //计算秒
@@ -241,43 +139,10 @@
       },
       // 滑动会请求数据
       async onLoad() {
->>>>>>> qa
       },
       // 返回上一级
       leftClick() {
         this.$router.go(-1)
-<<<<<<< HEAD
-
-      },
-      // 上拉刷新
-      async onRefresh(){
-        // 获取数据
-        let that=this;
-        that.datax=[];
-        let {code, data, coupons ,pagination} = await getProductCoup({
-          page:1,
-          coupon_id:this.objid
-        })
-        if (code === 0) {
-          that.datax=data;
-          that.coupons=coupons;
-          that.pagination=pagination;   // 加载状态结束
-          that.isLoading = false
-          // 数据全部加载完成
-          if (!that.pagination.more) {
-            that.prodFinished = true
-          }
-
-        }
-
-      },
-
-
-    }
-  }
-</script>
-
-=======
       },
       // 上拉刷新
       async onRefresh(){
@@ -297,7 +162,6 @@
     margin-top: 6px;
   }
 </style>
->>>>>>> qa
 <style type="text/scss" lang="scss" scoped>
   .product-list{
     .product-item{
@@ -441,10 +305,6 @@
     font-size: 24px;
     color: #FBA35A;
   }
-<<<<<<< HEAD
-=======
-
->>>>>>> qa
   .setspecial{
     display: inline-block;
     margin-right: 10px;
