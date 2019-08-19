e0121a0d55cd40f8666775ca366fe1760f68d672
c_illusion <670199384@qq.com>
2019-08-19 15:02:53 +0800

    添加trace_code

--- plugins/router/index.js
+++ plugins/router/index.js
@@ -1,12 +1,16 @@
 import NProgress from 'nprogress'
 import '@/assets/style/nprogress.css'
 import apiConf from '@/apiConf.env.js'
+import {getSessionStore, setSessionStore} from '@/assets/js/utils'
 
 export default ({
   app
 }) => {
   app.router.beforeEach((to, from, next) => {
     console.log('router name: ', to, from)
+    let traceCode = getSessionStore('traceCode') || ''
+    setSessionStore('traceCode', to.query.trace_code || traceCode)
+
     let reg=/(\/){2,}/
     let path = to.fullPath.replace(reg, '/')
     if(reg.test(to.fullPath)) {
