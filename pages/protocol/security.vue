19aa37eb7b6ac7ecf8c2d2bd8524a6395c03005a
linpeng <1186248752@qq.com>
2020-01-19 14:48:28 +0800

    预定协议修改

--- pages/protocol/security.vue
+++ pages/protocol/security.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="security-page">
-    <div class="security out-layer-con"
-      v-html="$t('html.security')">
+<div class="con" :class="!isApp?'appPadding':''">
+  <div class="header" v-if="!isApp">
+        <van-nav-bar
+          class="bar-shadow"
+          :title="$t('alonePage.selfActive')"
+          @click-left="onClickLeft"
+          left-arrow
+          v-if="!isApp"
+        >
+        </van-nav-bar>
+      </div>
+    <div class="security-page">
+      <div class="security out-layer-con"
+        v-html="$t('html.security')">
+      </div>
     </div>
   </div>
 </template>
@@ -14,18 +26,42 @@
         title: '高风险项目'
       }
     },
+     data() {
+    return {
+      isApp: this.$route.query.platform,
+    }
+  },
+  methods: {
+    onClickLeft() {
+     let href = window.location.href.slice(-1)
+      if(href == '#'){
+        this.$router.go(-2)
+      } else {
+        this.$router.go(-1)
+      }
+    }
+  }
   }
 </script>
 
 <style type="text/scss" lang="scss" scoped>
+.appPadding{
+    padding-top: 92px;
+}
+.header{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+  }
   .security-page {
     .out-layer-con {
-      position: relative;
+     /*  position: relative; */
       border-top: 2px solid #d8d8d8;
     }
     .security {
       font-size: 28px;
-      line-height: 34px;
+      line-height: 38px;
       padding: 32px;
       h2.title {
         text-align: center;
