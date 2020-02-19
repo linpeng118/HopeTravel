29d875423b6129fd4a91000a0668618ac052804f
weibaocheng@tourscool.com <weibaocheng@tourscool.com>
2020-02-19 18:19:22 +0800

    login

--- components/dialog/login.vue
+++ components/dialog/login.vue
@@ -1,82 +1,88 @@
 <template>
   <van-popup v-model="isShow"
-    class="login-dialog-comp"
-    :overlay="true"
-    :style="{width: '86%'}">
+             class="login-dialog-comp"
+             position="bottom"
+             :overlay="true">
     <div class="btn-close"
-      @click="closeDlg">
+         @click="closeDlg">
       <van-icon name="cross" />
     </div>
     <div class="dialog-content">
-      <login-page-comp isDialog
-        showRegistTip
-        showLoginTip />
+      <LoginComp :loginTitle="$t('partcailComp.subscribeLogin')"
+                 :regTitle="$t('partcailComp.subscribeLogin')"
+                 isDialog
+                 showRegistTip
+                 showLoginTip />
     </div>
   </van-popup>
 </template>
 
 <script>
-  import {mapMutations} from 'vuex'
-  import LoginPageComp from '@/components/partcail/login'
-  import {getLocalStore, setLocalStore} from '@/assets/js/utils'
+import {mapMutations} from 'vuex'
+import LoginComp from '@/components/partcail/login/login'
+import {getLocalStore, setLocalStore} from '@/assets/js/utils'
 
-  export default {
-    components: {
-      LoginPageComp,
+export default {
+  components: {
+    LoginComp,
+  },
+  props: {
+    // 是否显示弹窗
+    proIsShow: {
+      type: Boolean,
+      default: true,
     },
-    props: {
-      // 是否显示弹窗
-      proIsShow: {
-        type: Boolean,
-        default: true,
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    isShow: {
+      get() {
+        return this.proIsShow
+      },
+      set(val) {
+        this.$emit('callToggleShow', val)
       },
     },
-    data() {
-      return {}
+  },
+  methods: {
+    ...mapMutations({
+      vxToggleLoginDlg: 'login/toggleDialog',
+    }),
+    // 关闭弹窗
+    closeDlg() {
+      this.vxToggleLoginDlg(false)
     },
-    computed: {
-      isShow: {
-        get() {
-          return this.proIsShow
-        },
-        set(val) {
-          this.$emit('callToggleShow', val)
-        }
-      },
-    },
-    methods: {
-      ...mapMutations({
-        vxToggleLoginDlg: 'login/toggleDialog'
-      }),
-      // 关闭弹窗
-      closeDlg() {
-        this.vxToggleLoginDlg(false)
-      },
-    },
-  }
+  },
+}
 </script>
 <style>
-  .login-dialog-comp+.van-overlay{
-    /*z-index: 1999!important;*/
-  }
+.van-overlay {
+  /* z-index: 4000 !important; */
+}
 </style>
 <style lang="scss" scoped>
-  .login-dialog-comp {
-    /*z-index: 2000!important;*/
-    border-radius: 12px;
-    .btn-close {
-      position: absolute;
-      top: 0;
-      right: 10px;
-      padding: 8px;
-      color: #d8d8d8;
-      .van-icon {
-        font-size: 36px;
-      }
-    }
-    .dialog-content {
-      padding: 110px 60px;
-      text-align: center;
+.login-dialog-comp {
+  width: auto;
+  // z-index: 4000 !important;
+  left: 48px;
+  right: 48px;
+  bottom: 0;
+  border-top-left-radius: 32px;
+  border-top-right-radius: 32px;
+  .btn-close {
+    position: absolute;
+    top: 28px;
+    right: 38px;
+    color: rgb(71, 30, 30);
+    .van-icon {
+      font-size: 36px;
     }
   }
+  .dialog-content {
+    padding: 54px 46px 300px;
+    text-align: center;
+  }
+}
 </style>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,98 +1,195 @@
 <template>
   <div class="login-page">
-    <!-- 头部 -->
-    <login-header :rightText="rightText"
-      @callOnLeft="btnLeft"
-      @callOnRight="btnRight" />
-    <!-- 登录组件全套 -->
-    <div class="login-all-comp">
-      <login-page-comp />
+
+    <!-- 页面关闭按钮 -->
+    <van-icon @click="btnLeft()"
+              name="cross"
+              class="close-btn" />
+
+    <!-- 登录 -->
+    <login-comp v-if="formStyle==1"
+                class="login-comp-wrap"
+                ref="loginComp"
+                :loginTitle="$t('partcailComp.loginTips')"
+                :loginSubTitle="$t('partcailComp.loginSubTips')"
+                :regTitle="$t('partcailComp.toursLogin')"
+                :isAgree="isAgree"
+                @toRegist="formStyle=2"
+                @loginCallBack="loginCallBack()" />
+
+    <!-- 注册 -->
+    <regist-comp v-if="formStyle==2"
+                 class="regist-comp-wrap"
+                 ref="regComp"
+                 :regTitle="$t('partcailComp.emailReg')"
+                 @toLogin="formStyle=1"
+                 @registCallBack="registCallBack()" />
+
+    <!-- 协议 -->
+    <div class="agreement-warpper"
+         @click="toggleAgreeProp()">
+      <img src="../../assets/imgs/login/checked.png"
+           alt=""
+           v-if="isAgree">
+      <img src="../../assets/imgs/login/check.png"
+           alt=""
+           v-else>
+      同意
+      <span @click.stop="onAgreement">{{$t('partcailComp.AgreementName')}}</span>
+      和
+      <span @click.stop="$router.push({
+        path: '/protocol/xifan',
+      })">{{$t('partcailComp.privacy')}}</span>
     </div>
+
+    <!-- <div class="login-all-comp">
+      <login-page-comp />
+    </div> -->
   </div>
 </template>
 
 <script>
-  import {mapState, mapMutations} from 'vuex'
-  import {PAGE_TYPE} from '@/assets/js/consts/login'
-  import loginHeader from '@/components/header/loginHeader'
-  import LoginPageComp from '@/components/partcail/login'
+import {mapState, mapMutations} from 'vuex'
+import LoginComp from '@/components/partcail/login/login'
+import RegistComp from '@/components/partcail/login/regist'
+import {getProfile} from '@/api/profile'
 
-  export default {
-    components: {
-      loginHeader,
-      LoginPageComp,
-    },
-    data() {
-      return {
-        // 显示的页面类型
-        PAGE_TYPE,
-        // 重定向地址
-        redirect: this.$route.query.redirect ? decodeURIComponent(this.$route.query.redirect) : '',
+export default {
+  components: {
+    LoginComp,
+    RegistComp,
+  },
+  data() {
+    return {
+      redirect: this.$route.query.redirect ? decodeURIComponent(this.$route.query.redirect) : '', // 重定向地址
+
+      formStyle: 2, //1.登录 2.注册
+      isAgree: true, //是否同意协议
+    }
+  },
+  methods: {
+    // 头部按钮（左）【返回】
+    btnLeft() {
+      // 如果游重定向地址
+      if (this.redirect) {
+        this.$router.replace({
+          path: this.redirect,
+          query: {
+            isLogin: false,
+          },
+        })
+      } else {
+        this.$router.go(-1)
       }
     },
-    computed: {
-      ...mapState({
-        vxPage: state => state.login.page // 登录展示哪个页面
-      }),
-      // 头部右侧文字
-      rightText() {
-        if (this.vxPage === PAGE_TYPE.LOGIN) {
-          return '注册'
-        }
-        return '登录'
+    //协议同意切换
+    toggleAgreeProp() {
+      switch (this.formStyle) {
+        case 1:
+          this.$refs.loginComp.switchAgree(this.isAgree)
+          break
+        case 2:
+          this.$refs.regComp.switchAgree(this.isAgree)
+          break
       }
+      this.isAgree = !this.isAgree
     },
-    destroyed() {
-      this.vxChangePage(PAGE_TYPE.LOGIN);
-    },
-    methods: {
-      ...mapMutations({
-        vxChangePage: 'login/changePage'
-      }),
-      // 头部按钮（左）【返回】
-      btnLeft() {
-        switch (this.vxPage) {
-          case PAGE_TYPE.CHANGE:
-            this.vxChangePage(PAGE_TYPE.FORGET)
-            break;
-          default:
-            // 如果游重定向地址
-            if (this.redirect) {
-              this.$router.replace({
-                path: this.redirect,query:{
-                  isLogin:false
-                }
-              })
-            } else {
-              let href = window.location.href.slice(-1)
-              if(href == '#'){
-                this.$router.go(-2)
-              } else {
-                this.$router.go(-1)
-              }
-            }
-            break;
-        }
-      },
-      // 头部按钮（右）
-      btnRight() {
-        if (this.vxPage === PAGE_TYPE.LOGIN) {
-          this.vxChangePage(PAGE_TYPE.REGIST)
+    // 登陆回调
+    async loginCallBack() {
+      console.log('loginCallBack')
+      try {
+        fbq('track', 'Lead')
+      } catch (error) {
+        console.log(error)
+      }
+      return
+      // 弹窗登录/页面登录
+      await this.getUserInfo()
+      console.log(this.isDialog, this.redirect)
+
+      if (this.isDialog) {
+        this.vxToggleLoginDlg(false)
+        // this.$router.go(0) 手机端有些浏览器不能刷新
+        location.reload()
+      } else {
+        if (this.redirect) {
+          this.$router.replace({
+            path: this.redirect,
+            query: {
+              isLogin: true,
+            },
+          })
         } else {
-          this.vxChangePage(PAGE_TYPE.LOGIN)
+          console.log('进来咯')
+
+          this.$router.push({path: '/personal'})
         }
-      },
+      }
     },
-  }
+    // 获取到用户信息
+    async getUserInfo() {
+      let {data, code} = await getProfile()
+      console.log(data, code)
+
+      if (code === 0) {
+        console.log(1111)
+
+        this.vxSetProfile(data)
+      } else {
+        this.vxSetProfile({})
+      }
+      console.log(this.$store.state.profile.profile)
+    },
+    //注册
+    registHandle() {},
+    // 注册回调
+    registCallBack() {
+      try {
+        fbq('track', 'Lead')
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-  .login-page {
-    height: 100%;
-    min-height: 100vh;
-    text-align: center;
-    .login-all-comp {
-      padding: 136px 76px 0;
+.login-page {
+  height: 100%;
+  min-height: 100vh;
+  padding: 174px 72px 0;
+  position: relative;
+  .close-btn {
+    position: absolute;
+    left: 62px;
+    top: 36px;
+    color: #000;
+    font-size: 36px;
+  }
+  // 登录
+  .login-all-comp {
+    padding: 212px 72px 0;
+  }
+
+  // 协议
+  .agreement-warpper {
+    width: 100%;
+    position: fixed;
+    left: 0;
+    bottom: 136px;
+    font-size: 24px;
+    color: #888;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    img {
+      width: 44px;
+      height: 44px;
+    }
+    span {
+      color: rgba(57, 158, 246, 1);
     }
   }
+}
 </style>
