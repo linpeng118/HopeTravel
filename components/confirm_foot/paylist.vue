67f9d4eb13142b380b41db098222f7634e2f7561
victor <904810917@qq.com>
2020-02-20 16:04:17 +0800

    价格明细

--- components/confirm_foot/paylist.vue
+++ components/confirm_foot/paylist.vue
@@ -1,80 +1,88 @@
 <!--订单和行程的页脚组件-->
 <template>
   <section>
-
     <div class="paylist">
       <p class="paytitle">
         {{$t('costDetail')}}
-        <van-icon @click="closepops()" name="cross"/>
+        <van-icon @click="closepops()"
+                  name="cross" />
       </p>
       <ul>
         <li class="paysection">
-          <p class="payitem" v-if="payData.base_price">
+          <p class="payitem"
+             v-if="payData.base_price">
             <span>{{$t('confirmFootComp.basicTourFee')}}:</span>
             <span>{{payData.base_price}}</span>
           </p>
-          <p class="payitem2" >
+          <p class="payitem2">
             <span>{{$t('adult')}}:</span>
-            <span>{{payData.adult}} {{$t('person')}}</span>
+            <span>{{payData.adult}}{{$t('person')}}x{{payData.adult_price}}</span>
           </p>
-          <p class="payitem2" v-if="payData.child!=0">
+          <p class="payitem2"
+             v-if="payData.child!=0">
             <span>{{$t('child')}}:</span>
             <span>{{payData.child}} {{$t('person')}}</span>
           </p>
         </li>
-        <li class="paysection" v-if="payData.dfc.price">
-          <p class="payitem" >
+        <li class="paysection"
+            v-if="payData.dfc.price">
+          <p class="payitem">
             <span>{{payData.dfc.name}}</span>
             <span>
               {{payData.dfc.price}}</span>
           </p>
         </li>
-        <li class="paysection" v-if="payData.attributes_selected">
-          <p class="payitem" >
+        <li class="paysection"
+            v-if="payData.attributes_selected">
+          <p class="payitem">
             <span>{{$t('confirmFootComp.travelCost')}}:</span>
             <span>{{payData.attributes_selected.total_price}}</span>
           </p>
           <template v-if="payData.attributes_selected.items&&payData.attributes_selected.items.length">
-            <p class="payitem2" v-for="(item,ind2) in payData.attributes_selected.items" :key="ind2">
+            <p class="payitem2"
+               v-for="(item,ind2) in payData.attributes_selected.items"
+               :key="ind2">
               <span>{{item.name}}</span>
               <span>
-                 <i :style="item.prefix=='+'?'color:#D51D28':'color:#aaa;text-decoration:line-through'">{{item.prefix}} </i>
+                <i :style="item.prefix=='+'?'color:#D51D28':'color:#aaa;text-decoration:line-through'">{{item.prefix}} </i>
                 {{item.price}}</span>
             </p>
           </template>
 
         </li>
 
-        <li class="paysection" v-if="payData.discount">
+        <li class="paysection"
+            v-if="payData.discount">
           <p class="payitem">
             <span>{{$t('confirmFootComp.discount')}}:</span>
             <span :style="'color:#aaa;text-decoration:line-through'">-{{payData.discount}}</span>
           </p>
-          <p class="payitem2" v-if="payData.points&&showmili=='1'" >
+          <p class="payitem2"
+             v-if="payData.points&&showmili=='1'">
             <span>{{$t('confirmFootComp.riceGrains')}}</span>
             <span style="text-decoration:line-through;color:#aaa;"><i :style="'color:#aaa'">-</i>
-                {{payData.points.discount}}</span>
+              {{payData.points.discount}}</span>
           </p>
-          <p class="payitem2" v-if="payData.agent.discount && payData.agent.discount!='' && payData.agent.discount.substring(1) > 0" >
+          <p class="payitem2"
+             v-if="payData.agent.discount && payData.agent.discount!='' && payData.agent.discount.substring(1) > 0">
             <span>{{$t('confirmFootComp.unionSave')}}</span>
             <span><i :style="'color:#aaa'">-</i>
-                {{payData.agent.discount}}</span>
+              {{payData.agent.discount}}</span>
           </p>
 
-          <p class="payitem2" v-if="payData.coupons" >
+          <p class="payitem2"
+             v-if="payData.coupons">
             <span>{{$t('coupons')}}</span>
             <span style="text-decoration:line-through;color:#aaa;"><i :style="'color:#aaa'">-</i>
-                {{payData.coupons.save}}</span>
+              {{payData.coupons.save}}</span>
           </p>
         </li>
-
 
       </ul>
       <p class="payall">
         <span>{{$t('confirmFootComp.priceTotal')}}</span>
         <span>{{payData.total_price}}</span>
       </p>
-
 
     </div>
   </section>
@@ -84,26 +92,24 @@
 
   export default {
     props: {
-
       payData: Object,
       showmili: String,
-
     },
 
-    data() {
+    data () {
       return {}
     },
     watch: {},
-    created() {
+    created () {
 
     },
-    mounted() {
+    mounted () {
 
-      // console.log(this.payData)
+      console.log(this.payData)
 
     },
     methods: {
-      closepops() {
+      closepops () {
         this.$emit("closepops", 'false')
       }
 
@@ -116,7 +122,6 @@
 </script>
 
 <style scoped>
-
   .paylist {
     width: 750px;
     background: rgba(255, 255, 255, 1);
@@ -124,7 +129,6 @@
     overflow: hidden;
     border-radius: 24px 24px 0px 0px;
     padding-bottom: 40px;
-
   }
 
   .paytitle {
@@ -133,7 +137,7 @@
     height: 106px;
     line-height: 106px;
     padding: 0 32px;
-    border-bottom: 2px solid #E4E4E4;
+    border-bottom: 2px solid #e4e4e4;
     font-weight: bold;
     text-align: left;
   }
@@ -149,12 +153,11 @@
     box-sizing: border-box;
     height: 94px;
     margin-bottom: 120px;
-    border-top: 2px solid #E4E4E4;
-
+    border-top: 2px solid #e4e4e4;
   }
 
   .payall span:nth-child(1) {
-    color: #A1A1A1;
+    color: #a1a1a1;
     font-size: 24px;
     display: inline-block;
     float: left;
@@ -162,7 +165,7 @@
   }
 
   .payall span:nth-child(2) {
-    color: #FF5454;
+    color: #ff5454;
     display: inline-block;
     line-height: 94px;
     float: right;
@@ -170,7 +173,7 @@
   }
 
   .paysection {
-    border-top: 2px solid #E4E4E4;
+    border-top: 2px solid #e4e4e4;
   }
 
   .payitem {
@@ -194,7 +197,7 @@
   }
 
   .payitem span:nth-child(2) {
-    color: #FB605D;
+    color: #fb605d;
     display: inline-block;
     line-height: 78px;
     float: right;
@@ -227,6 +230,4 @@
     line-height: 60px;
     float: right;
   }
-
-
 </style>
