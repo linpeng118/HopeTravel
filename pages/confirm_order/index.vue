435e4beeec8d1f64605ea4ede95fe32695510cd7
linpeng <1186248752@qq.com>
2020-02-28 14:38:42 +0800

    区号国家函数顺序对换

--- pages/confirm_order/index.vue
+++ pages/confirm_order/index.vue
@@ -448,12 +448,14 @@
         console.log(this.getCountryCode);
         
         if(!this.getCountryCode['热门']||(this.getCountryCode['热门']&&this.getCountryCode['热门'].length==0)){
-          this.gotCountry();
+         
           this.gotQuhao();
+           this.gotCountry();
         } 
         if(!getLocalStore('tourscool_countryCode_vuex')){
-        this.gotCountry();
+        
         this.gotQuhao();
+        this.gotCountry();
       }   
       }, 20)
     },//I don't know why anotherone set setTimeout
